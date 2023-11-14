02220ec3d5ba54fc18d7b1db5fa16a0d1c2eaa14
Emil Kondov <122922427+EmilKondov@users.noreply.github.com>
2023-11-14 14:27:22 +0200

    Update 2. Find the Capitals.py
    Added second way to run the code using enumerate instead range()

--- More exercise (LOOPS)- Basic Syntax, Conditional Statements and Loops/2. Find the Capitals.py
+++ More exercise (LOOPS)- Basic Syntax, Conditional Statements and Loops/2. Find the Capitals.py
@@ -8,3 +8,16 @@ for current_character in range(len(given_string)):
   else:
     continue
 print(list_upper_character)
+
+
+### SECOND OPTION USING  ### ENUMERATE INSTEAD range() #####
+
+given_string = input()
+
+list_upper_character = []
+
+for index, character in enumerate(given_string):
+  if character.isupper():
+    list_upper_character.append(index)
+
+print(list_upper_character)
