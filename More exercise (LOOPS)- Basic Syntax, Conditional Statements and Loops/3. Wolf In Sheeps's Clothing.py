80982ada5f4ba59e70522fe214593b5504618a0d
Emil Kondov <e.kondov96@gmail.com>
2023-11-15 23:57:54 +0200

    Created new Directory - For Lecture number 2
    Completed task 1

---
+++ More exercise (LOOPS)- Basic Syntax, Conditional Statements and Loops/3. Wolf In Sheeps's Clothing.py
@@ -0,0 +1,6 @@
+animals = input()
+animals_list = animals.split(", ")
+number_of_animals = len(animals_list)
+
+for i in range(animals_list):
+    print(i)
--- More exercise (LOOPS)- Basic Syntax, Conditional Statements and Loops/Commands.py
+++ More exercise (LOOPS)- Basic Syntax, Conditional Statements and Loops/Commands.py
@@ -1,3 +1,6 @@
 # str.isupper() - to check and indicate if a characher is upper case
 # str.islower() - o check and indicate if a characher is lower case
 # list.append() - to add object to a list
+# sorted()
+
+# input.split() or str.spli(", ") - Даден текст се трансофрмира в списък като в скобите посочваме с какво да са разделени елементите на списъка
