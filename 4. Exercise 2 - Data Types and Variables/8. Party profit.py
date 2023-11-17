b4b6315c11d297fd23e05d2e95f01aa5cdd3bd2b
Emil Kondov <122922427+EmilKondov@users.noreply.github.com>
2023-11-17 11:52:49 +0200

    Completed Task8
    8. Party Profit

---
+++ 4. Exercise 2 - Data Types and Variables/8. Party profit.py
@@ -0,0 +1,21 @@
+from math import floor
+
+group_size = int(input())
+days = int(input())
+
+total_coins = 0
+
+for day in range(1, days + 1):
+    if day % 10 == 0:
+        group_size -= 2
+    if day % 15 ==0:
+        group_size += 5
+    if day % 3 == 0:
+        total_coins -= group_size * 3
+    if day % 5 == 0:
+        total_coins += group_size * 20
+        if day % 3 ==0:
+            total_coins -= group_size * 2
+    total_coins += 50 - group_size * 2
+coins_per_companinon = floor(total_coins / group_size)
+print(f"{group_size} companions received {coins_per_companinon} coins each.")
