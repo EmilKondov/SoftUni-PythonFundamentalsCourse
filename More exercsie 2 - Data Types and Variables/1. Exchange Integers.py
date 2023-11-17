19e638102bc15b25b99415065467593903220167
Emil Kondov <122922427+EmilKondov@users.noreply.github.com>
2023-11-17 15:45:28 +0200

    Create 1. Exchange Integers.py

---
+++ More exercsie 2 - Data Types and Variables/1. Exchange Integers.py
@@ -0,0 +1,11 @@
+number1 = int(input())
+number2 = int(input())
+a = number2
+b = number1
+
+print("Before:")
+print(f"a = {number1}")
+print(f"b = {number2}")
+print("After:")
+print(f"a = {a}")
+print(f"b = {b}")
