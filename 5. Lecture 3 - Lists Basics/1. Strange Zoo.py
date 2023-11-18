88a64ccd92901891346dfaf3f86cc6c97b3170a6
Emil Kondov <e.kondov96@gmail.com>
2023-11-18 20:17:45 +0200

    Update 1. Strange Zoo.py

--- 5. Lecture 3 - Lists Basics/1. Strange Zoo.py
+++ 5. Lecture 3 - Lists Basics/1. Strange Zoo.py
@@ -1,10 +1,21 @@
-body_parts_lst = []
+#body_parts_lst = []
+#
+#for current_part in range(3):
+#    current_part = input()
+#    body_parts_lst.append(current_part)
+#
+#body_parts_lst[0], body_parts_lst[2] = body_parts_lst[2], body_parts_lst[0]
+#print(body_parts_lst)
+#
+#
+#
 
-for current_part in range(3):
-    current_part = input()
-    body_parts_lst.append(current_part)
+tail = input()
+body = input()
+head = input()
 
-body_parts_lst[0], body_parts_lst[2] = body_parts_lst[2], body_parts_lst[0]
-print(body_parts_lst)
+meerkat = [tail, body, head]
 
+meerkat[0], meerkat[2] = meerkat[2], meerkat[0]
 
+print(meerkat)
