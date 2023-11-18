5d0a79aac9eaf7d268f13c6694a3414c37158fd4
Emil Kondov <e.kondov96@gmail.com>
2023-11-18 23:51:57 +0200

    Completed Task 1

--- 6. Exercise 3 - List Basics/1. Invert Values.py
+++ 6. Exercise 3 - List Basics/1. Invert Values.py
@@ -1,2 +1,8 @@
-import numpy as np
+list_of_numbers = input().split()
 
+oposite_numbers = []
+
+for element in list_of_numbers:
+    current_number = -int(element)
+    oposite_numbers.append(current_number)
+print(oposite_numbers)
