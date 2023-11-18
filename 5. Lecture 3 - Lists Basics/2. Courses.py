a79c38c59530a19b224ce5e59c9278f2ccfdeb49
Emil Kondov <e.kondov96@gmail.com>
2023-11-18 20:24:25 +0200

    Create 2. Courses.py

---
+++ 5. Lecture 3 - Lists Basics/2. Courses.py
@@ -0,0 +1,8 @@
+given_number_n = int(input())
+
+list_of_courses = []
+
+for name_current_course in range(given_number_n):
+    name_current_course = input()
+    list_of_courses.append(name_current_course)
+print(list_of_courses)
