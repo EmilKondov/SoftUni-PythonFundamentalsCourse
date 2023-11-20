a819ef705dcb76d4f26f112aa6b5d3fb66bea577
Emil Kondov <e.kondov96@gmail.com>
2023-11-20 22:51:50 +0200

    Create 7. Easter Gifts.py

---
+++ 6. Exercise 3 - List Basics/7. Easter Gifts.py
@@ -0,0 +1,12 @@
+gifts = input().split('')
+current_command = input()
+
+while current_command != "No Money":
+    if current_command == "OutOfStock":
+        pass
+    elif current_command == "Required":
+        pass
+    elif current_command == "JustInCase":
+        pass
+    current_command = input()
+
