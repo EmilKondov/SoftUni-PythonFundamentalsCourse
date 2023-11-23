42cf913a3a768c060701f05d31c5cbd6767acf4a
Emil Kondov <e.kondov96@gmail.com>
2023-11-23 21:28:09 +0200

    Update 5. Even Numbers.py

--- 8. Exercise 4 - Functions/5. Even Numbers.py
+++ 8. Exercise 4 - Functions/5. Even Numbers.py
@@ -12,14 +12,22 @@
 ##### SECOND WAY ####
 
 
-def is_even(number):
-    if number % 2 ==0:
-        return number
+#def is_even(number):
+#    if number % 2 ==0:
+#        return number
+#
+#numbers_as_string = input().split()
+#numbers_as_int = []
+#
+#for current_number in numbers_as_string:
+#    numbers_as_int.append(int(current_number))
+#result = list(filter(is_even, numbers_as_int))
+#print(result)
+#
 
-numbers_as_string = input().split()
-numbers_as_int = []
+#########  THIRD WAY ################
 
-for current_number in numbers_as_string:
-    numbers_as_int.append(int(current_number))
-result = list(filter(is_even, numbers_as_int))
+list_of_numbers = list(map(int, input().split()))
+is_even = lambda x: x % 2 == 0
+result = list(filter(is_even, list_of_numbers))
 print(result)
