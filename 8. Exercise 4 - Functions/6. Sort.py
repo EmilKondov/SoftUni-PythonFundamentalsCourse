248e1b81535476a09c8b64be8bedf115c3c7ff6f
Emil Kondov <e.kondov96@gmail.com>
2023-11-24 23:54:54 +0200

    Update 6. Sort.py
    Second way with function just for the exercise

--- 8. Exercise 4 - Functions/6. Sort.py
+++ 8. Exercise 4 - Functions/6. Sort.py
@@ -6,3 +6,10 @@
 
 
 
+###### SECOND WAY WITH FUNCTION ####
+
+def sorted_sequence(given_sequence):
+    return sorted(given_sequence)
+
+sequence_of_numbers = list(map(int, input().split()))
+print(sorted_sequence(sequence_of_numbers))
