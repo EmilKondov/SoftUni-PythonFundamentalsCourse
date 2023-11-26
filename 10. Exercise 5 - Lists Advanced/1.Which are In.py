fe532459b2333af8c63bc4988f9304ca2e2c4e5a
Emil Kondov <e.kondov96@gmail.com>
2023-11-26 20:53:24 +0200

    Update 1.Which are In.py
    Second way by using comprehension

--- 10. Exercise 5 - Lists Advanced/1.Which are In.py
+++ 10. Exercise 5 - Lists Advanced/1.Which are In.py
@@ -1,9 +1,18 @@
+#first_sequence = input().split(", ")
+#second_sequence = input().split(", ")
+#substrings = []
+#for word in first_sequence:
+#    for other_word in second_sequence:
+#        if word in other_word:
+#            substrings.append(word)
+#            break
+#print(substrings)
+#
+
+
+##### Second way by using comprehension ######
+
 first_sequence = input().split(", ")
 second_sequence = input().split(", ")
-substrings = []
-for word in first_sequence:
-    for other_word in second_sequence:
-        if word in other_word:
-            substrings.append(word)
-            break
-print(substrings)
+substring = [word for word in first_sequence if any(word in other_word for other_word in second_sequence)]
+print(substring)
