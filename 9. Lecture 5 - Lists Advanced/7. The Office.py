7d73ae08fe05f8ce46a253050b2b2c8c1b24fd2b
Emil Kondov <e.kondov96@gmail.com>
2023-11-26 20:36:51 +0200

    Update 7. The Office.py

--- 9. Lecture 5 - Lists Advanced/7. The Office.py
+++ 9. Lecture 5 - Lists Advanced/7. The Office.py
@@ -2,6 +2,5 @@ employees_happiness = input().split()
 improvement_factor = int(input())
 
 employees = list(map(lambda x: int(x) * improvement_factor, employees_happiness))
-filtered_employees = filter(employees)
-
+filtered = list
 print(employees)
