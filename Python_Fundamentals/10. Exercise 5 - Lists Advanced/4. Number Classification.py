49e390503d329b7a97f69b9aad9229a28487c5be
Emil Kondov <e.kondov96@gmail.com>
2023-11-29 00:15:25 +0200

    Update 4. Number Classification.py

--- Python_Fundamentals/10. Exercise 5 - Lists Advanced/4. Number Classification.py
+++ Python_Fundamentals/10. Exercise 5 - Lists Advanced/4. Number Classification.py
@@ -1,5 +1,4 @@
 list_of_numbers = input().split(", ")
 
-positive_numbers = ("Positive:"[int(positive_number) for number in list_of_numbers.split(", ") if number > 0])
-
-print(positive_numbers)
+positive = [number for number in list_of_numbers if int(number) >= 0]
+print(positive)
