cbf625f46c4f0cc618e9db3b7672cb757d943ff2
Emil Kondov <e.kondov96@gmail.com>
2023-11-29 10:29:56 +0200

    Update 4. Number Classification.py

--- Python_Fundamentals/10. Exercise 5 - Lists Advanced/4. Number Classification.py
+++ Python_Fundamentals/10. Exercise 5 - Lists Advanced/4. Number Classification.py
@@ -1,4 +1,10 @@
-list_of_numbers = input().split(", ")
+numbers = input().split(", ")
+positive = [number for number in numbers if int(number) >= 0]
+negative = [number for number in numbers if int(number) < 0]
+even = [number for number in numbers if int(number) % 2 == 0]
+odd = [number for number in numbers if int(number) % 2]
 
-positive = [number for number in list_of_numbers if int(number) >= 0]
-print(positive)
+print(f"Positive: {', '.join(positive)}")
+print(f"Negative: {', '.join(negative)}")
+print(f"Even: {', '.join(even)}")
+print(f"Odd: {', '.join(odd)}")
