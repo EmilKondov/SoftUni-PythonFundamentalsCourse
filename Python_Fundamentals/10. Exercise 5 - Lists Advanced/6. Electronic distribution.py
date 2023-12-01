2af35d01c279750730c1782804fd1d5e1b9a02ae
Emil Kondov <122922427+EmilKondov@users.noreply.github.com>
2023-12-01 17:44:53 +0200

    Update 6. Electronic distribution.py

--- Python_Fundamentals/10. Exercise 5 - Lists Advanced/6. Electronic distribution.py
+++ Python_Fundamentals/10. Exercise 5 - Lists Advanced/6. Electronic distribution.py
@@ -1,14 +1,28 @@
-number_of_electrons = int(input())
+#number_of_electrons = int(input())
+#
+#filled_shells = []
+#
+#
+#for n in range(1, number_of_electrons +1):
+#    if number_of_electrons > 0:
+#        max_electrons_in_shell = 2 * n **2
+#        if max_electrons_in_shell > number_of_electrons:
+#            filled_shells.append(number_of_electrons)
+#            break
+#        number_of_electrons -= max_electrons_in_shell
+#        filled_shells.append(max_electrons_in_shell)
+#print(filled_shells)
+#
 
-filled_shells = []
 
-
-for n in range(1, number_of_electrons +1):
-    if number_of_electrons > 0:
+def elctrons_in_shell(count_of_electrons: int):
+    for n in range(1, count_of_electrons +1):
         max_electrons_in_shell = 2 * n **2
-        if max_electrons_in_shell > number_of_electrons:
-            filled_shells.append(number_of_electrons)
-            break
-        number_of_electrons -= max_electrons_in_shell
-        filled_shells.append(max_electrons_in_shell)
-print(filled_shells)
+        if max_electrons_in_shell > count_of_electrons:
+            
+    pass
+
+
+
+number_of_electrons = int(input())
+elctrons_in_shell(number_of_electrons)
