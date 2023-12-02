24b9eddacde1418e1ea44cc0f9347c3ee2d317d2
Emil Kondov <e.kondov96@gmail.com>
2023-12-02 18:26:32 +0200

    Update 8. Decipher This!.py
    to be completed

---
+++ Python_Fundamentals/10. Exercise 5 - Lists Advanced/8. Decipher This!.py
@@ -0,0 +1,15 @@
+import math
+secret_message = input().split()
+
+ascii_table_code = []
+
+for word in secret_message:
+    for character in word:
+        if 
+            ascii_table_code.append(int(character))
+
+
+
+
+
+print(secret_message)
