ec291a6662ccebeb703824318a326b2f3ce8fe52
Emil Kondov <e.kondov96@gmail.com>
2023-12-02 23:02:05 +0200

    Update 8. Decipher This!.py

--- Python_Fundamentals/10. Exercise 5 - Lists Advanced/8. Decipher This!.py
+++ Python_Fundamentals/10. Exercise 5 - Lists Advanced/8. Decipher This!.py
@@ -4,12 +4,15 @@ secret_message = input().split()
 ascii_table_code = []
 
 for word in secret_message:
+    list_of_numbers = []
+    list_of_letters = []
     for character in word:
-        if 
-            ascii_table_code.append(int(character))
-
-
-
-
-
-print(secret_message)
+        if character.isdigit():
+            list_of_numbers.append(character)
+        elif character.isalpha():
+            list_of_letters.append(character)
+    first_letter = chr(int("".join(list_of_numbers)))
+    list_of_letters[0], list_of_letters[-1] = list_of_letters [-1], list_of_letters[0]
+    letters_after_first = "".join(list_of_letters)
+    deciphered_word = first_letter + letters_after_first
+    print(deciphered_word, end=" ")
