53315b54c2dab8320bdf6ccd5aa83fd039d92c6c
Emil Kondov <e.kondov96@gmail.com>
2023-12-09 17:08:17 +0200

    Update 8. Vehicle.py

--- Python_Fundamentals/13. Exercise 6 - Objects and classes/8. Vehicle.py
+++ Python_Fundamentals/13. Exercise 6 - Objects and classes/8. Vehicle.py
@@ -17,14 +17,14 @@ class Vehicle:
 
 
     def sell(self):
-        if self.owner:
+        if self.owner: # == if self.owner is not None:
             self.owner = None
         else:
             return f"Vehicle has no owner"
 
 
     def __repr__(self):
-        if self.owner:
+        if self.owner: # == if self.owner is not None:
             return f"{self.model} {self.type} is owned by: {self.owner}"
         else:
             return f"{self.model} {self.type} is on sale: {self.price}"
