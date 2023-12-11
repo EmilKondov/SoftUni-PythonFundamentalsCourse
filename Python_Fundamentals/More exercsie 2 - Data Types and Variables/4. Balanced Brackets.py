438fffcd3660a55e909ba069a08547db1f4be3d7
Emil Kondov <e.kondov96@gmail.com>
2023-12-11 23:20:01 +0200

    Update 4. Balanced Brackets.py

--- Python_Fundamentals/More exercsie 2 - Data Types and Variables/4. Balanced Brackets.py
+++ Python_Fundamentals/More exercsie 2 - Data Types and Variables/4. Balanced Brackets.py
@@ -1,1 +1,13 @@
 number_of_lines = int(input())
+
+brackets_counter = 0
+
+for element in range(number_of_lines):
+    element = input()
+    if "(" in element or ")" in element:
+        brackets_counter += 1
+
+if brackets_counter % 2 == 0:
+    print("BALANCED")
+else:
+    print("UNBALANCED")
