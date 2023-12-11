2e834785ff453499b2beafd2e968e7f2d084104b
Emil Kondov <e.kondov96@gmail.com>
2023-12-11 23:31:47 +0200

    Update 3. Decrypting messages.py

--- Python_Fundamentals/More exercsie 2 - Data Types and Variables/3. Decrypting messages.py
+++ Python_Fundamentals/More exercsie 2 - Data Types and Variables/3. Decrypting messages.py
@@ -1,2 +1,9 @@
 key = int(input())
 number_of_lines = int(input())
+message = []
+
+for value in range(number_of_lines):
+    value = ord(input()) + key
+    message.append(chr(value))
+print("".join(message))
+
