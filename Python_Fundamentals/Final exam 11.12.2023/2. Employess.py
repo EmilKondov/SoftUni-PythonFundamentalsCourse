5a96293757ac8f1d89b076a7416ee54d94bf5fdb
Emil Kondov <122922427+EmilKondov@users.noreply.github.com>
2023-12-11 17:20:20 +0200

    Create 2. Employess.py

---
+++ Python_Fundamentals/Final exam 11.12.2023/2. Employess.py
@@ -0,0 +1,21 @@
+import re
+
+
+def decrypt_input(input_string):
+    pattern = re.compile(
+        r"([A-Z][a-z]{2,})\s([A-Z][a-z]{2,})(#*)([A-Z][a-z]+(?:&[A-Z][a-z]+){0,2})\d{2}([A-Z][a-zA-Z0-9]+) (JSC|Ltd\.)")
+    match = pattern.match(input_string)
+
+    if match:
+        name = f"{match.group(1)} {match.group(2)}"
+        position = match.group(4).replace('&', ' ')
+        company = f"{match.group(5)} {match.group(6)}"
+        return f"{name} is {position} at {company}"
+
+
+n = int(input())
+for employee in range(n):
+    input_line = input()
+    result = decrypt_input(input_line)
+    if result:
+        print(result)
