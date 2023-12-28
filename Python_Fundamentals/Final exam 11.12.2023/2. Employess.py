6864698f01c8715da7e2011ab4040202c28b447e
Emil Kondov <e.kondov96@gmail.com>
2023-12-28 12:47:46 +0200

    Update 2. Employess.py

--- Python_Fundamentals/Final exam 11.12.2023/2. Employess.py
+++ Python_Fundamentals/Final exam 11.12.2023/2. Employess.py
@@ -1,6 +1,5 @@
 import re
 
-
 def decrypt_input(input_string):
     pattern = re.compile(
         r"([A-Z][a-z]{2,})\s([A-Z][a-z]{2,})(#*)([A-Z][a-z]+(?:&[A-Z][a-z]+){0,2})\d{2}([A-Z][a-zA-Z0-9]+) (JSC|Ltd\.)")
