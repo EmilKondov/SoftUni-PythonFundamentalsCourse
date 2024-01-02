6091f67bbb15726d2e1244e2abd009a8f4243093
Emil Kondov <122922427+EmilKondov@users.noreply.github.com>
2024-01-02 17:11:16 +0200

    Create 5. Tic tac toe.py

---
+++ Python_Fundamentals/More exercise 3 - Lists Basics/5. Tic tac toe.py
@@ -0,0 +1,13 @@
+def player_one():
+
+
+first_line = input().split()
+second_line = input().split()
+third_line = input().split()
+
+print(type(first_line))
+print(type(second_line))
+print(type(third_line))
+print(first_line)
+print(second_line)
+print(third_line)
