36ddb57b7448ff334ed7f359e13846a5a916add3
Emil Kondov <122922427+EmilKondov@users.noreply.github.com>
2024-01-03 11:50:26 +0200

    Update 5. Tic tac toe.py

--- Python_Fundamentals/More exercise 3 - Lists Basics/5. Tic tac toe.py
+++ Python_Fundamentals/More exercise 3 - Lists Basics/5. Tic tac toe.py
@@ -1,13 +1,41 @@
-def player_one():
-
-
 first_line = input().split()
 second_line = input().split()
 third_line = input().split()
 
-print(type(first_line))
-print(type(second_line))
-print(type(third_line))
-print(first_line)
-print(second_line)
-print(third_line)
+winner = 0
+
+if "1" == first_line[0] == first_line[1] == first_line[2] or \
+            "1" == second_line[0] == second_line[1] == second_line[2] or \
+            "1" == third_line[0] == third_line[1] == third_line[2] or \
+            "1" == first_line[0] == second_line[0] == third_line[0] or \
+            "1" == first_line[1] == second_line[1] == third_line[1] or \
+            "1" == first_line[2] == second_line[2] == third_line[2] or \
+            "1" == first_line[0] == second_line[1] == third_line[2] or \
+            "1" == third_line[0] == second_line[1] == first_line[2]:
+        winner = 1
+elif "2" == first_line[0] == first_line[1] == first_line[2] or \
+            "2" == second_line[0] == second_line[1] == second_line[2] or \
+            "2" == third_line[0] == third_line[1] == third_line[2] or \
+            "2" == first_line[0] == second_line[0] == third_line[0] or \
+            "2" == first_line[1] == second_line[1] == third_line[1] or \
+            "2" == first_line[2] == second_line[2] == third_line[2] or \
+            "2" == first_line[0] == second_line[1] == third_line[2] or \
+            "2" == first_line[2] == second_line[1] == third_line[0]:
+        winner = 2
+
+
+if winner == 1:
+    print(f"First player won")
+elif winner == 2:
+    print(f"Second player won")
+else:
+    print("Draw!")
+
+
+# You will receive a field of a tic-tac-toe game in three lines containing numbers, separated by a single space.
+# Legend:
+# • 0 - empty space
+# • 1 - first player move
+# • 2 - second player move
+# Find out who the winner is. If the first player wins, print "First player won". If the second player wins, print
+# "Second player won". Otherwise, print "Draw!".
