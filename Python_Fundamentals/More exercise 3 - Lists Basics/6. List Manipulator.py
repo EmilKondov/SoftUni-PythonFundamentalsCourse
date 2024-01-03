e1ce5a28adea863bfa12ebea9dcaab30c8969cd7
Emil Kondov <e.kondov96@gmail.com>
2024-01-03 23:51:03 +0200

    Update 6. List Manipulator.py

--- Python_Fundamentals/More exercise 3 - Lists Basics/6. List Manipulator.py
+++ Python_Fundamentals/More exercise 3 - Lists Basics/6. List Manipulator.py
@@ -8,33 +8,56 @@ def exchange(numbers, index):
     if index > len(numbers) or index < 0:
         print("Invalid index")
     line_of_numbers = numbers[index:] + numbers[:index]
-    print(line_of_numbers)
     return line_of_numbers
 
 def max_even_odd(numbers, even_or_odd):
     if even_or_odd == "even":
-        for i in range(len(numbers) -1, -1, -1):
-            if numbers[i] % 2 != 0:
-                numbers.pop(i)
-        numbers.sort()
-        print(len(numbers))
-        return len(numbers)
+        for i in range(len(numbers)):
+            if numbers[i] % 2 == 0:
+                max_even_number = numbers[i]
+                if max_even_number < numbers[i + 1] and numbers[i + 1] != numbers[-1]:
+                    continue
+                elif numbers[i] != numbers[-1]:
+                    max_even_number = i + 1
+                    break
+                else:
+                    max_even_number = i
+        return max_even_number
     elif even_or_odd == "odd":
         for i in range(len(numbers)):
             if numbers[i] % 2 != 0:
                 max_odd_number = numbers[i]
-                if max_odd_number < numbers[i + 1]:
+                if max_odd_number < numbers[i + 1] and numbers[i + 1] != numbers[-1]:
                     continue
                 else:
-                    print(i + 1)
-        numbers.sort()
-        print(len(numbers))
-        return len(numbers)
-def min_even():
-    pass
-
-def min_odd():
-    pass
+                    max_odd_number = i
+                    break
+        return max_odd_number
+def min_even_odd(numbers, even_or_odd):
+    if even_or_odd == "even":
+        for i in range(len(numbers)):
+            if numbers[i] % 2 == 0:
+                min_even_number = numbers[i]
+                if min_even_number < numbers[i + 1] and numbers[i + 1] != numbers[-1]:
+                    continue
+                elif numbers[i] != numbers[-1]:
+                    min_even_number = i + 1
+                    return min_even_number
+                    break
+                else:
+                    max_even_number = i
+                    return max_even_number
+                    break
+        return "No matches"
+    elif even_or_odd == "odd":
+        odd_numbers = []
+        for i in range(len(numbers)):
+            if numbers[i] % 2 != 0 and numbers[i] != 1:
+                odd_numbers.append(numbers[i])
+            else:
+                odd_numbers = "[]"
+                return odd_numbers
+                break
 
 def firs_even():
     pass
@@ -55,18 +78,16 @@ command = input()
 while command != "end":
     command = command.split()
     order = command[0]
-    if order == "exchange":
+    if order == 'exchange':
         index = int(command[1])
         line_of_numbers = exchange(line_of_numbers, index)
     elif order == "max":
         even_odd = command[1]
-        max_number = max_even_odd(line_of_numbers, even_odd)
+        max_number_index = max_even_odd(line_of_numbers, even_odd)
     elif order == "min":
         even_odd = command[1]
-        if even_odd == "even":
-            pass
-        elif even_odd =="odd":
-            pass
+        min_number_index = min_even_odd(line_of_numbers, even_odd)
+        print(min_number_index)
     elif order == "first":
         count = command[1]
         even_odd = command[2]
@@ -74,5 +95,5 @@ while command != "end":
         count = command[1]
         even_odd = command[2]
     command = input()
-print(line_of_numbers)
 
+print(line_of_numbers)
