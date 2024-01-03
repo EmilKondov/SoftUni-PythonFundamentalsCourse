5de2edbd616ea7b5b23f687387c127e63a8d4459
Emil Kondov <122922427+EmilKondov@users.noreply.github.com>
2024-01-03 11:50:30 +0200

    Update 2. Destination Mapper.py

--- Python_Fundamentals/Final Exam Prep/2. Destination Mapper.py
+++ Python_Fundamentals/Final Exam Prep/2. Destination Mapper.py
@@ -1,20 +1,20 @@
-import re
-
-def valid_destinations_function(destinations_data):
-
-    pattern = r"(\=|\/)([A-Z][A-Za-z]{2,})\1"
-    travel_points = 0
-    valid_destinations = []
-    result = re.finditer(pattern, destinations_data)
-
-    if not result:
-        print("Destinations:\nTravel Points: 0")
-    else:
-        for current_destination in result:
-            valid_destinations.append(current_destination.group(2))
-            travel_points += len(current_destination.group(2))
-        print(f"Destinations: {', '.join(valid_destinations)}\nTravel Points: {travel_points}")
-
-
-data = input()
-valid_destinations_function(data)
+# import re
+#
+# def valid_destinations_function(destinations_data):
+#
+#     pattern = r"(\=|\/)([A-Z][A-Za-z]{2,})\1"
+#     travel_points = 0
+#     valid_destinations = []
+#     result = re.finditer(pattern, destinations_data)
+#
+#     if not result:
+#         print("Destinations:\nTravel Points: 0")
+#     else:
+#         for current_destination in result:
+#             valid_destinations.append(current_destination.group(2))
+#             travel_points += len(current_destination.group(2))
+#         print(f"Destinations: {', '.join(valid_destinations)}\nTravel Points: {travel_points}")
+#
+#
+# data = input()
+# valid_destinations_function(data)
