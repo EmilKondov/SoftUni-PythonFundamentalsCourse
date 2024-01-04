45457f23668536de8736f3fc2cb238cac1f1ca89
Emil Kondov <e.kondov96@gmail.com>
2024-01-04 21:43:02 +0200

    Update 6.1.List Manipulator - 2.py

--- Python_Fundamentals/More exercise 3 - Lists Basics/6.1.List Manipulator - 2.py
+++ Python_Fundamentals/More exercise 3 - Lists Basics/6.1.List Manipulator - 2.py
@@ -2,7 +2,6 @@ import sys
 
 line_of_numbers = [int(x) for x in input().split()]
 command = input().split()
-mutated_line = []
 
 while command[0] != 'end':
     even_numbers = [int(even) for even in line_of_numbers if even % 2 == 0]
@@ -11,14 +10,14 @@ while command[0] != 'end':
     min_number = sys.maxsize
     value_index = 0
     is_found = False
-    counter = 0
+    count = 0
     first_even = []
     first_odd = []
     last_even = []
     last_odd = []
-
     if command[0] == "exchange":
         index = int(command[1])
+        mutated_line = []
         if index > len(line_of_numbers) or index < 0:
             print("Invalid index")
         else:
@@ -32,7 +31,7 @@ while command[0] != 'end':
                         max_number = value
                         value_index = index
                         is_found = True
-        else:
+        elif "odd" in command[1]:
             for index, value in enumerate(line_of_numbers):
                 if value % 2 != 0:
                     if value >= max_number:
@@ -43,7 +42,6 @@ while command[0] != 'end':
             print(value_index)
         else:
             print("No matches")
-
     elif command[0] == "min":
         if "even" in command[1]:
             for index, value in enumerate(line_of_numbers):
@@ -63,16 +61,18 @@ while command[0] != 'end':
             print(value_index)
         else:
             print("No matches")
-
     elif command[0] == "first":
-        current_count, even_odd = command[1],command[2]
-        if even_odd == "even":
+        count, even_odd = int(command[1]), command[2]
+        if count > len(line_of_numbers):
+            print("Invalid count")
+        elif even_odd == "even":
             for index, value in enumerate(even_numbers):
-                if index < int(current_count):
+                if index < int(count):
                     first_even.append(value)
+                    is_found = True
         else:
             for index, value in enumerate(odd_numbers):
-                if index < int(current_count):
+                if index < int(count):
                     first_odd.append(value)
                     is_found = True
                 else:
@@ -81,17 +81,16 @@ while command[0] != 'end':
             print(first_even)
         elif is_found and even_odd == "odd":
             print(first_odd)
-
     elif command[0] == "last":
-        current_count, even_odd = int(command[1]), command[2]
-        if int(current_count) > len(line_of_numbers):
+        count, even_odd = int(command[1]), command[2]
+        if int(count) > len(line_of_numbers):
             print("Invalid count")
         if even_odd == "even":
-            print(even_numbers[-current_count:])
+            print(even_numbers[-count:])
         else:
-            if int(current_count) > len(line_of_numbers):
+            if int(count) > len(line_of_numbers):
                 print("Invalid count")
             else:
-                print(odd_numbers[-current_count:])
+                print(odd_numbers[-count:])
     command = input().split()
 print(line_of_numbers)
