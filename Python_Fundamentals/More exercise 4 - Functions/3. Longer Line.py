4a1ef597aa93f94058bc84d08919363b68ab59fb
Emil Kondov <e.kondov96@gmail.com>
2024-01-05 23:58:54 +0200

    Create 3. Longer Line.py

---
+++ Python_Fundamentals/More exercise 4 - Functions/3. Longer Line.py
@@ -0,0 +1,73 @@
+import math
+
+def long_line(a, b , c, d):
+    if a > c:
+
+    else:
+
+    if b > d:
+        pass
+    else:
+
+
+
+x1 = float(input())
+y1 = float(input())
+a = abs(x1) + abs(y1)
+
+x2 = float(input())
+y2 = float(input())
+b = abs(x2) + abs(y2)
+
+x3 = float(input())
+y3 = float(input())
+c = abs(x3) + abs(y3)
+
+x4 = float(input())
+y4 = float(input())
+d = abs(x4) + abs(y4)
+
+
+if a > c:
+    point_of_line = x1, y1
+else:
+    point_of_line = x3, y3
+
+if b > d:
+    second_point_of_line = x2, y2
+else:
+    second_point_of_line = x4, y4
+    
+x = 0
+y = 0
+
+if abs(x1) <= abs(x2) or \
+        abs(x1) <= abs(x2) or \
+        abs(x1) <= abs(x3) or \
+        abs(x1) <= abs(x4):
+    x = x1
+elif abs(x2) <= abs(x3) or \
+        abs(x2) <= abs(x4):
+    x = x2
+elif abs(x3) <= abs(x4):
+    x = x3
+else:
+    x = x2
+
+if abs(y1) <= abs(y2) or \
+        abs(y1) <= abs(y2) or \
+        abs(y1) <= abs(y3) or \
+        abs(y1) <= abs(y4):
+    y = y1
+elif abs(y2) <= abs(y3) or \
+        abs(y2) <= abs(y4):
+    y = y2
+elif abs(y3) <= abs(y4):
+    y = y3
+else:
+    y = y2
+
+longer_line = long_line(a, b, c, d)
+print(f"({math.floor(x)}, {math.floor(y)})")
+
+
