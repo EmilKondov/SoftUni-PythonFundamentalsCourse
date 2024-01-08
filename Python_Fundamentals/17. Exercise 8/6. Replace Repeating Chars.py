555e3a415976f851b0b2247b0af2791c29dfa77c
Emil Kondov <122922427+EmilKondov@users.noreply.github.com>
2024-01-08 18:58:38 +0200

    Update 6. Replace Repeating Chars.py

--- Python_Fundamentals/17. Exercise 8/6. Replace Repeating Chars.py
+++ Python_Fundamentals/17. Exercise 8/6. Replace Repeating Chars.py
@@ -2,13 +2,10 @@ given_string = input()
 final_text = ""
 last_character = ""
 
-
 for ch in given_string:
-    current_character = ch
-    if current_character in given_string:
-        if ch != last_character:
-            final_text += ch
-            last_character = current_character
-        else:
-            continue
+    if ch != last_character:
+        final_text += ch
+        last_character = ch
+    else:
+        continue
 print(final_text)
