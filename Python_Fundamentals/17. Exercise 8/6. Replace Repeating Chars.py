cddf65076c4a772309b57019748b715256d1cc0f
Emil Kondov <122922427+EmilKondov@users.noreply.github.com>
2024-01-08 10:32:48 +0200

    Update 6. Replace Repeating Chars.py

--- Python_Fundamentals/17. Exercise 8/6. Replace Repeating Chars.py
+++ Python_Fundamentals/17. Exercise 8/6. Replace Repeating Chars.py
@@ -4,4 +4,11 @@ last_character = ""
 
 
 for ch in given_string:
-    
+    current_character = ch
+    if current_character in given_string:
+        if ch != last_character:
+            final_text += ch
+            last_character = current_character
+        else:
+            continue
+print(final_text)
