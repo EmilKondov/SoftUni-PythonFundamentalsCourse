f3834cfe5bee858f0a6933d8d98f907c2c12c8cc
Emil Kondov <e.kondov96@gmail.com>
2024-01-09 22:19:38 +0200

    Update Reverse string using Stack.py

--- Python_Advanced/1. Lecture 1 - Lists as Stacks and Queues/Reverse string using Stack.py
+++ Python_Advanced/1. Lecture 1 - Lists as Stacks and Queues/Reverse string using Stack.py
@@ -1,7 +1,16 @@
+# text = list(input())
+# stack = []
+#
+# for i in range(len(text)):
+#     stack.append(text.pop())
+#
+# print("".join(stack))
+#
+
+
+### Second most simple way ###
+
 text = list(input())
-stack = []
 
-for i in range(len(text)):
-    stack.append(text.pop())
-
-print("".join(stack))
+while text:
+    print(text.pop(), end= "")
