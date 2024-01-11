d15e4da0bbdd1e351fffce3993ffdbd9900abec3
Emil Kondov <e.kondov96@gmail.com>
2024-01-11 23:21:49 +0200

    Update 1. Reverse numbers.py

--- Python_Advanced/2. Exercsie 1 - Lists as Stack and Queues/1. Reverse numbers.py
+++ Python_Advanced/2. Exercsie 1 - Lists as Stack and Queues/1. Reverse numbers.py
@@ -1,6 +1,12 @@
-given_int = list(input())
-stack = []
+from collections import deque
+# given_int = deque(input().split())
+#
+# for _ in range(len(given_int)):
+#     print(given_int.pop(), end=" ")
+#
 
-while given_int:
-    stack.append(given_int.pop())
-print("".join(stack))
+numbers = deque(input().split())
+
+numbers.reverse()
+
+print(*numbers)
