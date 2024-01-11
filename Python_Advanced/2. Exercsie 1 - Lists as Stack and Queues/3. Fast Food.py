3eea6843fec8be81186ac6a4350a9514bad1dff9
Emil Kondov <e.kondov96@gmail.com>
2024-01-11 23:51:47 +0200

    Update 3. Fast Food.py

--- Python_Advanced/2. Exercsie 1 - Lists as Stack and Queues/3. Fast Food.py
+++ Python_Advanced/2. Exercsie 1 - Lists as Stack and Queues/3. Fast Food.py
@@ -1,14 +1,32 @@
-from collections import deque
+#from collections import deque
+#
+# food_quantity = int(input())
+# orders = deque(input().split())
+#
+# print(max(int(x) for x in orders))
+#
+# while orders:
+#     if food_quantity >= int(orders[0]):
+#         order = orders.popleft()
+#         food_quantity -= int(order)
+#     else:
+#         print(f"Orders left:", *orders)
+#         break
+# else:
+#     print("Orders complete")
 
-food_quantity = int(input())
+### Second solution ### 
+
+from collections import deque
+food = int(input())
 orders = deque(input().split())
 
 print(max(int(x) for x in orders))
 
-while orders:
-    if food_quantity >= int(orders[0]):
+for order in orders.copy():
+    if food >= int(order):
         order = orders.popleft()
-        food_quantity -= int(order)
+        food -= int(order)
     else:
         print(f"Orders left:", *orders)
         break
