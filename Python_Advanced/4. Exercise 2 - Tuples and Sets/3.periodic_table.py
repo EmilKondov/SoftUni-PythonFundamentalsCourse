9ec91b80e8fe27f81eeebf17711414a036406aff
Emil Kondov <e.kondov96@gmail.com>
2024-01-15 18:51:02 +0200

    Update 3.periodic_table.py

--- Python_Advanced/4. Exercise 2 - Tuples and Sets/3.periodic_table.py
+++ Python_Advanced/4. Exercise 2 - Tuples and Sets/3.periodic_table.py
@@ -1,9 +1,21 @@
-count_of_lines = int(input())
-elements_list = []
+# count_of_lines = int(input())
+# elements_list = []
+#
+# for _ in range(count_of_lines):
+#     elements = input().split()
+#     elements_list.extend(elements)
+#
+# for element in set(elements_list):
+#     print(element)
 
-for _ in range(count_of_lines):
-    elements = input().split()
-    elements_list.extend(elements)
 
-for element in set(elements_list):
-    print(element)
+
+### Second solution ###
+
+table = set()
+
+for _ in range(int(input())):
+    for el in input().split():
+        table.add(el)
+
+print(*table, sep="\n")
