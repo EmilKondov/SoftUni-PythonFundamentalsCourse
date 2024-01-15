5b5883037b0c867957c0723f6c5dd55babfa8e31
Emil Kondov <e.kondov96@gmail.com>
2024-01-15 18:32:01 +0200

    Update 2.sets_of_elements.py

--- Python_Advanced/4. Exercise 2 - Tuples and Sets/2.sets_of_elements.py
+++ Python_Advanced/4. Exercise 2 - Tuples and Sets/2.sets_of_elements.py
@@ -1,17 +1,27 @@
-sets_lenght = [int(x) for x in input().split()]
-first_set_lenght, second_set_lenght = sets_lenght
-sets_items = []
-for item in range(first_set_lenght + second_set_lenght):
-    sets_items.append(int(input()))
-
-first_set = set(sets_items[:first_set_lenght])
-second_set = set(sets_items[first_set_lenght:])
-
-# for element in first_set.intersection(second_set):
+# sets_lenght = [int(x) for x in input().split()]
+# first_set_lenght, second_set_lenght = sets_lenght
+# sets_items = []
+# for item in range(first_set_lenght + second_set_lenght):
+#     sets_items.append(int(input()))
+#
+# first_set = set(sets_items[:first_set_lenght])
+# second_set = set(sets_items[first_set_lenght:])
+#
+# # for element in first_set.intersection(second_set):
+# #     print(element)
+#
+#
+# ### Or by using intersection operator &
+#
+# for element in first_set & second_set:
 #     print(element)
 
 
-### Or by using intersection operator & 
+#### Second solution ####
 
-for element in first_set & second_set:
-    print(element)
+n, m = [int(x) for x in input().split()]
+
+first_set = {input() for _ in range(n)}
+second_set = {input() for _ in range(m)}
+
+print(*first_set.intersection(second_set), sep="\n")
