80ab917bd833bd4ef623ed3479143593a8baca96
Emil Kondov <e.kondov96@gmail.com>
2024-01-15 20:31:20 +0200

    Update 4.count_symbols.py

--- Python_Advanced/4. Exercise 2 - Tuples and Sets/4.count_symbols.py
+++ Python_Advanced/4. Exercise 2 - Tuples and Sets/4.count_symbols.py
@@ -1,9 +1,29 @@
-string_input = input()
-list_of_the_string = [character for character in string_input]
-input_characters = {}
+# string_input = input()
+# list_of_the_string = [character for character in string_input]
+# input_characters = {}
+#
+# for character in string_input:
+#     input_characters[character] = list_of_the_string.count(character)
+#
+# for char, count in sorted(input_characters.items()):
+#     print(f"{char}: {count} time/s")
+#
+#
+# ### Second solution ###
+#
+#
+# occurences = {}
+#
+# for symbol in input():
+#     occurences[symbol] = occurences.get(symbol, 0 ) + 1
+#
+# for letter, occurence in sorted(occurences.items()):  # (("a", 1), ("b", 3)) sorted returns us a tuple.
+#     print(f"{letter}: {occurence} time/s")
+#
 
-for character in string_input:
-    input_characters[character] = list_of_the_string.count(character)
+### Third solution ###
 
-for char, count in sorted(input_characters.items()):
-    print(f"{char}: {count} time/s")
+text = input()
+
+for symbol in sorted(set(text)):
+    print(f"{symbol}: {text.count(symbol)} time/s")
