ebd0870524cfeaa044b545c0f3604e4ce1d179d0
Emil Kondov <122922427+EmilKondov@users.noreply.github.com>
2024-01-15 17:24:13 +0200

    Update TEST.py

--- Python_Advanced/TEST.py
+++ Python_Advanced/TEST.py
@@ -1,3 +1,14 @@
-text = "My name is Peter"
-name = text[-5:]
-print(name)
+number_of_lines = int(input())
+
+sets = {}
+for given_range in range(number_of_lines):
+    list_of_ranges = [[int(x) for x in x.split(',')] for x in input().split('-')]
+    first_list_of_ranges = list_of_ranges[0]
+    second_list_of_ranges = list_of_ranges[1]
+    sets[given_range] = []
+    for number in range(first_list_of_ranges[0], first_list_of_ranges[-1] + 1):
+        sets[given_range].append(number)
+    for number in range(second_list_of_ranges[0], second_list_of_ranges[-1] + 1):
+        sets[given_range].append(number)
+
+
