95aaa4ca45d1c04cf9ffe33c73893e5e989cc759
Emil Kondov <e.kondov96@gmail.com>
2024-01-16 22:53:32 +0200

    Update 6.paint_colors.py

--- Python_Advanced/5. Exercise 3 - Stacks, Queues, Tuples and Sets/6.paint_colors.py
+++ Python_Advanced/5. Exercise 3 - Stacks, Queues, Tuples and Sets/6.paint_colors.py
@@ -1,24 +1,27 @@
 from collections import deque
 
 given_input = deque(input().split())
-colors = []
 possible_colors = {"red", "yellow", "blue", "orange", "purple", "green"}
 secondary_colors = {"orange": {"red", "yellow"},
                     "purple": {"red", "blue"},
-                    "green": {"yellow", "blue"},
-}
+                    "green": {"yellow", "blue"},}
+colors = []
+
 while given_input:
     left_part = given_input.popleft()
     right_part = given_input.pop() if given_input else ''
-    possible_colors = left_part + right_part
+
 
     for color in (left_part + right_part, right_part + left_part):
         if color in possible_colors:
             colors.append(color)
             break
-        else:
-            for el in (left_part[:-1], right_part[:-1]):
-                if el:
-                    given_input.insert(len(given_input) // 2, el)
+    else:
+        for el in (left_part[:-1], right_part[:-1]):
+            if el:
+                given_input.insert(len(given_input) // 2, el)
 
-
+for color in set(secondary_colors.keys()).intersection(colors):
+    if not secondary_colors[color].issubset(colors):
+        colors.remove(color)
+print(colors)
