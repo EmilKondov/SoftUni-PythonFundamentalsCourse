bee3ad1e4fc1031f8ed232eccf991cb5ce215379
Emil Kondov <122922427+EmilKondov@users.noreply.github.com>
2024-01-16 17:07:39 +0200

    Create 2.expression_evaluator.py

---
+++ Python_Advanced/5. Exercise 3 - Stacks, Queues, Tuples and Sets/2.expression_evaluator.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+string_input = deque(x for x in input().split())
+
+for element in string_input:
+    if element == "+":
+        pass
+    elif element == "-":
+        pass
+    elif element == "*":
+        pass
+    elif element == "/":
+        pass
+
+print(string_input)
