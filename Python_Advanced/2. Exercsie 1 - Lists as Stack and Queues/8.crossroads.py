65b0d0f19ded0ad860c0c312a9c5913fe2db0d86
Emil Kondov <122922427+EmilKondov@users.noreply.github.com>
2024-01-17 17:53:34 +0200

    Update 8.crossroads.py
    Only 53 out of 100 in Judge.
    
    To be debugged.

--- Python_Advanced/2. Exercsie 1 - Lists as Stack and Queues/8.crossroads.py
+++ Python_Advanced/2. Exercsie 1 - Lists as Stack and Queues/8.crossroads.py
@@ -1,31 +1,37 @@
 from collections import deque
 green_light_duration = int(input())
 free_window_duration = int(input())
-cars = deque()
-stuck_cars = []
 
 current_light_duration = green_light_duration
 current_free_window_duration = free_window_duration
 passed_cars_counter = 0
 
-while True:
+crash = False
+
+while current_light_duration != 0:
     command = input()
     if command == "END":
         break
     if command == "green":
         current_light_duration = green_light_duration
-
+        current_free_window_duration = free_window_duration
     else:
         current_car_passing_time = len(command)
         if current_car_passing_time <= current_light_duration:
             passed_cars_counter += 1
-            cars.append(command)
             current_light_duration -= current_car_passing_time
         else:
             characters_to_remove = current_light_duration
-            stuck_cars.append(command[:characters_to_remove])
-            if len(command) <= current_free_window_duration:
+            if len(command[characters_to_remove:]) < current_free_window_duration:
                 current_free_window_duration -= 1
-                stuck_cars.append(command)
-                cars.add(stuck_cars[0])
                 passed_cars_counter += 1
+                current_light_duration = 0
+            else:
+                character_hit = len(command[:characters_to_remove]) - current_free_window_duration
+                print("A crash happened!")
+                print(f"{command} was hit at {command[character_hit]}.")
+                crash= True
+                break
+if not crash:
+    print(f"""Everyone is safe.
+    {passed_cars_counter} total cars passed the crossroads.""")
