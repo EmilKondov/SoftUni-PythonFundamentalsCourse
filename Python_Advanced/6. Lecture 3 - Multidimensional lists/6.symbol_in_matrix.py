f284b334c0d8ba9473144b8b4ea1085c8e4bf8e5
Emil Kondov <e.kondov96@gmail.com>
2024-01-18 20:52:04 +0200

    Update 6.symbol_in_matrix.py

--- Python_Advanced/6. Lecture 3 - Multidimensional lists/6.symbol_in_matrix.py
+++ Python_Advanced/6. Lecture 3 - Multidimensional lists/6.symbol_in_matrix.py
@@ -14,10 +14,30 @@ for row in range(0, size):
         break
     for column in range(0, size):
         if matrix[row][column] == symbol:
-            location = (row, column)
+            location = (row, column)     # Or use directly exit
             found = True
 
 if found:
     print(location)
 else:
     print(f"{symbol} does not occur in the matrix")
+
+# Second way - using exit() ####
+
+size = int(input())
+matrix = []
+
+for row in range(0, size):
+    current_row = [x for x in input()]
+    matrix.append(current_row)
+
+symbol = input()
+location = ()
+
+for row in range(0, size):
+    for column in range(0, size):
+        if matrix[row][column] == symbol:
+            location = (row, column)     # Or use directly exit
+            exit()
+
+print(f"{symbol} does not occur in the matrix")
