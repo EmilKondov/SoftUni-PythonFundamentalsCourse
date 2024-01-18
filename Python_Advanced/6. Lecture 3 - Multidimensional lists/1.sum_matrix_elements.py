08107c1bd4b39bb4071bc25bbb6b84d88aeb6b6d
Emil Kondov <e.kondov96@gmail.com>
2024-01-18 18:49:50 +0200

    Update 1.sum_matrix_elements.py

--- Python_Advanced/6. Lecture 3 - Multidimensional lists/1.sum_matrix_elements.py
+++ Python_Advanced/6. Lecture 3 - Multidimensional lists/1.sum_matrix_elements.py
@@ -1,15 +1,33 @@
+# rows, columns = [int(x) for x in input().split(", ")]
+#
+# matrix_elements = []
+# sum = 0
+#
+# for row in range(rows):
+#     current_row = [int(x) for x in input().split(", ")]
+#     matrix_elements.append(current_row)
+#
+# for element in matrix_elements:
+#     for number in element:
+#         sum += number
+#
+# print(sum)
+# print(matrix_elements)
+
+
+### Second solution###
+
 rows, columns = [int(x) for x in input().split(", ")]
 
+total_amount = 0
 matrix_elements = []
-sum = 0
+
 
 for row in range(rows):
     current_row = [int(x) for x in input().split(", ")]
+    total_amount += sum(current_row)
     matrix_elements.append(current_row)
 
-for element in matrix_elements:
-    for number in element:
-        sum += number
 
-print(sum)
+print(total_amount)
 print(matrix_elements)
