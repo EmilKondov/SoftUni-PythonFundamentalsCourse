3529d9276749d4db46fd36b433092bb3bdf22e94
Emil Kondov <122922427+EmilKondov@users.noreply.github.com>
2024-01-18 12:41:18 +0200

    Create 5.primary_diagonal.py

---
+++ Python_Advanced/6. Lecture 3 - Multidimensional lists/5.primary_diagonal.py
@@ -0,0 +1,9 @@
+size = int(input())
+matrix = []
+
+for row in range(size):
+    current_row = [int(x) for x in input().split()]
+    matrix.append(current_row)
+
+sum_diagonal = sum(matrix[size - i - 1][size - i - 1] for i in range(size))
+print(sum_diagonal)
