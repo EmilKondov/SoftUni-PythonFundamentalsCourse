a32c86e15fce3479b2aafe65455b96c25c6671b2
Emil Kondov <122922427+EmilKondov@users.noreply.github.com>
2024-01-18 11:40:05 +0200

    Update 2.even_matrix.py

--- Python_Advanced/6. Lecture 3 - Multidimensional lists/2.even_matrix.py
+++ Python_Advanced/6. Lecture 3 - Multidimensional lists/2.even_matrix.py
@@ -5,5 +5,4 @@ matrix_elements = []
 for row in range(rows):
     current_row = [int(x) for x in input().split(", ") if int(x) % 2 == 0]
     matrix_elements.append(current_row)
-
 print(matrix_elements)
