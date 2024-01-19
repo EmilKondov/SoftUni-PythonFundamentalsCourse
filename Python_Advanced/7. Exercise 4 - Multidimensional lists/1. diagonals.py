ca57f4385e5d96b80dc6a414a2d2635ee1851087
Emil Kondov <122922427+EmilKondov@users.noreply.github.com>
2024-01-19 16:31:48 +0200

    Update 1. diagonals.py

--- Python_Advanced/7. Exercise 4 - Multidimensional lists/1. diagonals.py
+++ Python_Advanced/7. Exercise 4 - Multidimensional lists/1. diagonals.py
@@ -1,21 +1,32 @@
 size = int(input())
 matrix = [[int(x) for x in input().split(", ")] for x in range(size)]
 
+principal_diagonal = []
+secondary_diagonal = []
 
-primary_diagonal_sum = sum(matrix[size - i - 1][size - i - 1] for i in range(size))
-(matrix[size - i - 1] for i in range(size))
-(matrix[size - i - 1] for i in range(size -1, -1, -1))
+for row in range(size):
+    for col in range(size):
 
-print(list1)
-print(list2)
-#secondary_diagonal_sum = sum(matrix[size - i - 1][size - i - 1] for i in range(size))
+        if row == col:
+            principal_diagonal.append(matrix[row][col])
+
+        if ((row + col) == (size - 1)):
+            secondary_diagonal.append(matrix[row][col])
 
 
-# [3 - 0 - 1][3 - 0 - 1] = 9
-# [3 - 1 - 1][3 - 1 - 1] = 5
-# [3 - 2 - 1][3 - 2 - 1]  = 1
-#
-# [3 - 0 - 1][3 - 2 - 1] = 7
-# [3 - 1 - 1][3 - 1 - 1] = 5
-# [3 - 2 - 1][3 - 0 - 1] = 3
+print(f"Primary diagonal: {', '.join([str(x) for x in principal_diagonal])}. Sum: {sum(principal_diagonal)}")
+print(f"Secondary diagonal: {', '.join([str(y) for y in secondary_diagonal])}. Sum: {sum(secondary_diagonal)}")
+
+
+
+
+# for i in range(size):
+#     for j in range(size):
+#         if ((i + j) == (size - 1)):
+#             secondary_diagonal.append(matrix[i][j])
+
+
+
+
+#primary_diagonal_sum = sum(matrix[size - i - 1][size - i - 1] for i in range(size))
 
