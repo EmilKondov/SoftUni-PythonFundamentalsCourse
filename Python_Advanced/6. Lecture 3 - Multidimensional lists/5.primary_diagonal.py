7acbfa5ad2a5c60566cab181ae7961d9d50109e7
Emil Kondov <122922427+EmilKondov@users.noreply.github.com>
2024-01-19 15:09:40 +0200

    Update 5.primary_diagonal.py

--- Python_Advanced/6. Lecture 3 - Multidimensional lists/5.primary_diagonal.py
+++ Python_Advanced/6. Lecture 3 - Multidimensional lists/5.primary_diagonal.py
@@ -6,7 +6,22 @@ for row in range(size):
     matrix.append(current_row)
 
 sum_diagonal = sum(matrix[size - i - 1][size - i - 1] for i in range(size))
+
+for i in range(size - 1):
+    for j in range(size -1):
+        lower = matrix[i + 2][i]
+        central = matrix[i + 1][j + 1]
+        upper = matrix[i][j + 2]
+        sum_second_diagonal = lower + central + upper
+        break
+
 print(sum_diagonal)
+print(sum_second_diagonal)
+
+
+
+
+
 
 #### Second way ###
 #
