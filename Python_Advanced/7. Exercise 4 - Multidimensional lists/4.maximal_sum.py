5550f62a9fa03e129f4e4bee172ec574dba91849
Emil Kondov <e.kondov96@gmail.com>
2024-01-21 13:15:55 +0200

    Update 4.maximal_sum.py

--- Python_Advanced/7. Exercise 4 - Multidimensional lists/4.maximal_sum.py
+++ Python_Advanced/7. Exercise 4 - Multidimensional lists/4.maximal_sum.py
@@ -1,10 +1,20 @@
 rows, columns = [int(x) for x in input().split()]
-
 matrix = [[int(x) for x in input().split()] for row in range(rows)]
-sub_matrix = []
-current_row = []
 
-for row in range(3):
-    for column in range(3):
-        current_row.append(matrix[row][column])
-    sub_matrix.append(current_row)
+max_sum = float('-inf')
+biggest_matrix = []
+
+
+for row in range(rows - 2):
+    for col in range(columns -2):
+        first_row = matrix[row][col:col + 3]
+        second_row = matrix[row + 1][col:col + 3]
+        third_row = matrix[row + 2][col:col + 3]
+
+        current_sum = sum(first_row) + sum(second_row) + sum(third_row)
+
+        if current_sum > max_sum:
+            max_sum = current_sum
+            biggest_matrix = [first_row, second_row, third_row]
+print(f"Sum = {max_sum}")
+[print(*row) for row in biggest_matrix]
