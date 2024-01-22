12ef538318156bdae01f50e1f140af0917391ffb
Emil Kondov <e.kondov96@gmail.com>
2024-01-22 19:03:41 +0200

    Update 1.flatten_list.py

--- Python_Advanced/8. Exercise 5 - MDL/1.flatten_list.py
+++ Python_Advanced/8. Exercise 5 - MDL/1.flatten_list.py
@@ -1,8 +1,17 @@
-given_list = input().split("|")
+# given_list = input().split("|")
+#
+# sub_list = []
+#
+# for sub_string in given_list[::-1]:   # чрез слайсинг казвам да започне от последния елемент да върти цикъла в обратен ред.
+#     sub_list.extend(sub_string.split())
+#
+# print(*sub_list)
+#
 
-sub_list = []
 
-for sub_string in given_list[::-1]:   # чрез слайсинг казвам да започне от последния елемент да върти цикъла в обратен ред.
-    sub_list.extend(sub_string.split())
+### Solution 2
 
-print(*sub_list)
+numbers = [string.split() for string in input().split("|")]
+
+print(*[' '.join(sub_list) for sub_list in numbers[::-1] if sub_list])
+
