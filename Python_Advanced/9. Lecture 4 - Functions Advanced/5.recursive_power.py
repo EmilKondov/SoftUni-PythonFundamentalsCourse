5c59d9a9e2aeb1ab41f54bf0fd6d5aabbf1e9b54
Emil Kondov <e.kondov96@gmail.com>
2024-01-23 22:51:21 +0200

    Update 5.recursive_power.py

--- Python_Advanced/9. Lecture 4 - Functions Advanced/5.recursive_power.py
+++ Python_Advanced/9. Lecture 4 - Functions Advanced/5.recursive_power.py
@@ -1,9 +1,6 @@
 def recursive_power(number, power):
-    result = 1
-    if power == 0:
-        return result
-    result = number * recursive_power(number, power -1)
-    return result
-
+    if power == 1:
+        return number
+    return number * recursive_power(number, power -1)
 
 print(recursive_power(2, 10))
