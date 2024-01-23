1c39598bc25f45aae02baca33de87b8e555a344e
Emil Kondov <122922427+EmilKondov@users.noreply.github.com>
2024-01-23 15:30:51 +0200

    Update 3.knight_game.py

--- Python_Advanced/8. Exercise 5 - MDL/3.knight_game.py
+++ Python_Advanced/8. Exercise 5 - MDL/3.knight_game.py
@@ -1,22 +1,21 @@
 size = int(input())
-board = [[x for x in input()] for _ in range(size)]
+board = [list(input()) for _ in range(size)]
 
 possitions = (
      (-2, -1),
-     (-1, -2),
      (-2, 1),
      (-1, -2),
-     (1, -2),
-     (2, -1),
+     (-1, 2),
      (2, 1),
+     (2, -1),
+     (1, -2),
      (1, 2),
 )
-
 removed_knights = 0
-
 while True:
     max_attacks = 0
-    knights_with_most_attacks = 0
+    knights_with_most_attacks = []
+
     for row in range(size):
         for col in range(size):
             if board[row][col] == "K":
@@ -33,26 +32,11 @@ while True:
                 if attacks > max_attacks:
                     knights_with_most_attacks = [row, col]
                     max_attacks = attacks
-        if knights_with_most_attacks:
-            r, c = knights_with_most_attacks
-            board[r][c] = 0
-            removed_knights += 1
-        else:
-            break
-
+    if knights_with_most_attacks:
+        r, c = knights_with_most_attacks
+        board[r][c] = 0
+        removed_knights += 1
+    else:
+        break
+        
 print(removed_knights)
-
-
-
-
-
-
-
-
-
-# 5
-# 0K0K0
-# K000K
-# 00K00
-# K000K
-# 0K0K0
