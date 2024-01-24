d34870810c2e47dc505612d7611e175ef009ef70
Emil Kondov <122922427+EmilKondov@users.noreply.github.com>
2024-01-24 13:06:54 +0200

    Update file_for_tests.py

--- Python_Advanced/9. Lecture 4 - Functions Advanced/file_for_tests.py
+++ Python_Advanced/9. Lecture 4 - Functions Advanced/file_for_tests.py
@@ -17,5 +17,26 @@
 
 #2. Person Info
 
+# def get_info(name, age, town):
+#     return f"This is {name} from {town} and he is {age} years old"
+#
+#
+# print(get_info(**{"name": "George", "town": "Sofia", "age": 20}))
+
+#3.Cheese Showcase:
+
+def sorting_cheeses(**kwargs):
+    sorted_cheese = sorted(kwargs.items(), key=lambda x: (-len(x[1]), x[0]))
+
+    result = []
+
+    for (key, value) in sorted_cheese:
+        result.append(key)
+        value_list = sorted(value, reverse=True)
+        result += value_list
+    return "\n".join([str(x) for x in result])
+
+print(sorting_cheeses(Parmesan=[102, 120, 135],Camembert=[100, 100, 105, 500, 430],Mozzarella=[50, 125],))
+
 
 
