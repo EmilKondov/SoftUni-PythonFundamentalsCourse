85c5007993be1f1b5ccfad52abe2e97364e9dc28
Emil Kondov <122922427+EmilKondov@users.noreply.github.com>
2024-01-24 12:30:22 +0200

    Create file_for_tests.py

---
+++ Python_Advanced/9. Lecture 4 - Functions Advanced/file_for_tests.py
@@ -0,0 +1,5 @@
+def names(**kwargs):
+    for key, value in kwargs.items():
+        print(f"My first name is {key} and my second name is {value}")
+
+names(Emil="Kondov", Margarit="Markov", Zhenya="Kondova")
