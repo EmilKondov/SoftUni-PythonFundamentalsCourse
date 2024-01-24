aaffd7cb3c832f41eb517d9f3ee8def90099efa9
Emil Kondov <122922427+EmilKondov@users.noreply.github.com>
2024-01-24 12:35:21 +0200

    Update file_for_tests.py

--- Python_Advanced/9. Lecture 4 - Functions Advanced/file_for_tests.py
+++ Python_Advanced/9. Lecture 4 - Functions Advanced/file_for_tests.py
@@ -1,5 +1,21 @@
-def names(**kwargs):
-    for key, value in kwargs.items():
-        print(f"My first name is {key} and my second name is {value}")
+# def names(**kwargs):
+#     for key, value in kwargs.items():
+#         print(f"My first name is {key} and my second name is {value}")
+#
+# names(Emil="Kondov", Margarit="Markov", Zhenya="Kondova")
 
-names(Emil="Kondov", Margarit="Markov", Zhenya="Kondova")
+
+# 1.Multiplication function:
+#
+# def multiply(*args):
+#     sum = 1
+#     for num in args:
+#         sum *= num
+#     return sum
+#
+# print(multiply(1, 4, 5))
+
+#2. Person Info
+
+
+
