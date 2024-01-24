9a424fc34d99df536bfb9100a4e385e16c8c1cbf
Emil Kondov <122922427+EmilKondov@users.noreply.github.com>
2024-01-24 13:51:03 +0200

    Update file_for_tests.py

--- Python_Advanced/9. Lecture 4 - Functions Advanced/file_for_tests.py
+++ Python_Advanced/9. Lecture 4 - Functions Advanced/file_for_tests.py
@@ -26,8 +26,13 @@
 #3.Cheese Showcase:
 
 def sorting_cheeses(**kwargs):
-    sorted_cheese = sorted(kwargs.items(), key=lambda x: (-len(x[1]), x[0]))
-
+    sorted_cheese = sorted(kwargs.items(), key=lambda x: (-len(x[1]), x[0]))   # в аргумента на функцията (-len(x[1]), x[0]) е оградено с (), защото
+                                                                               # по този начин казваме сортирай първо по критерии ..., и после по. Тоест соритрай по брой на елементи, но
+                                                                               # ако броя на елементите е равен то тогава сортирай тези елементи по азбучен ред на тяхните ключове.
+    """                                                                        
+    С **kwargs получваме речник от данните, които сме получили, но в sorted(),
+     когато използваме .items() речникът се превръща във tuple от key-value pairs <=> kvp! 
+    """
     result = []
 
     for (key, value) in sorted_cheese:
@@ -35,8 +40,5 @@ def sorting_cheeses(**kwargs):
         value_list = sorted(value, reverse=True)
         result += value_list
     return "\n".join([str(x) for x in result])
-
 print(sorting_cheeses(Parmesan=[102, 120, 135],Camembert=[100, 100, 105, 500, 430],Mozzarella=[50, 125],))
-
-
-
+print(sorting_cheeses(Parmigiano=[165, 215],Feta=[150, 515],Brie=[150, 125]))
