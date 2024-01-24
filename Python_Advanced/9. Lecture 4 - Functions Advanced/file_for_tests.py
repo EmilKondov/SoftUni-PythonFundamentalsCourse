8e515f5be209f2a2796adc66125b5d889123cfba
Emil Kondov <122922427+EmilKondov@users.noreply.github.com>
2024-01-24 14:00:56 +0200

    Update file_for_tests.py

--- Python_Advanced/9. Lecture 4 - Functions Advanced/file_for_tests.py
+++ Python_Advanced/9. Lecture 4 - Functions Advanced/file_for_tests.py
@@ -25,20 +25,38 @@
 
 #3.Cheese Showcase:
 
-def sorting_cheeses(**kwargs):
-    sorted_cheese = sorted(kwargs.items(), key=lambda x: (-len(x[1]), x[0]))   # в аргумента на функцията (-len(x[1]), x[0]) е оградено с (), защото
-                                                                               # по този начин казваме сортирай първо по критерии ..., и после по. Тоест соритрай по брой на елементи, но
-                                                                               # ако броя на елементите е равен то тогава сортирай тези елементи по азбучен ред на тяхните ключове.
-    """                                                                        
-    С **kwargs получваме речник от данните, които сме получили, но в sorted(),
-     когато използваме .items() речникът се превръща във tuple от key-value pairs <=> kvp! 
-    """
-    result = []
+# def sorting_cheeses(**kwargs):
+#     sorted_cheese = sorted(kwargs.items(), key=lambda x: (-len(x[1]), x[0]))   # в аргумента на функцията (-len(x[1]), x[0]) е оградено с (), защото
+#                                                                                # по този начин казваме сортирай първо по критерии ..., и после по. Тоест соритрай по брой на елементи, но
+#                                                                                # ако броя на елементите е равен то тогава сортирай тези елементи по азбучен ред на тяхните ключове.
+#     """
+#     С **kwargs получваме речник от данните, които сме получили, но в sorted(),
+#      когато използваме .items() речникът се превръща във tuple от key-value pairs <=> kvp!
+#     """
+#     result = []
+#
+#     for (key, value) in sorted_cheese:
+#         result.append(key)
+#         value_list = sorted(value, reverse=True)
+#         result += value_list
+#     return "\n".join([str(x) for x in result])
+# print(sorting_cheeses(Parmesan=[102, 120, 135],Camembert=[100, 100, 105, 500, 430],Mozzarella=[50, 125],))
+# print(sorting_cheeses(Parmigiano=[165, 215],Feta=[150, 515],Brie=[150, 125]))
 
-    for (key, value) in sorted_cheese:
-        result.append(key)
-        value_list = sorted(value, reverse=True)
-        result += value_list
-    return "\n".join([str(x) for x in result])
-print(sorting_cheeses(Parmesan=[102, 120, 135],Camembert=[100, 100, 105, 500, 430],Mozzarella=[50, 125],))
-print(sorting_cheeses(Parmigiano=[165, 215],Feta=[150, 515],Brie=[150, 125]))
+
+#4 rectangle
+
+def rectangle(length, width):
+    if not isinstance(length, int) and not isinstance(width, int):
+        return f"Enter valid values!"
+
+    def area():
+        return length * width
+
+    def perimeter():
+        return 2 * (length + width)
+
+    return f"Rectangle area: {area()}\nRectangle perimeter: {perimeter()}"
+
+
+print(rectangle(2, 10))
