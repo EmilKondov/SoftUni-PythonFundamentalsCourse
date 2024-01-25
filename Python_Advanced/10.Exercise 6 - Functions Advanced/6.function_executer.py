2b7ba98bbe244675931ec9eb867bf564f148c360
Emil Kondov <e.kondov96@gmail.com>
2024-01-25 21:46:24 +0200

    Create 6.function_executer.py

---
+++ Python_Advanced/10.Exercise 6 - Functions Advanced/6.function_executer.py
@@ -0,0 +1,11 @@
+def func_executor(*action_data):
+    retu
+
+
+    def sum_numbers(num1, num2):
+        return num1 + num2
+
+    def multiply_numbers(num1, num2):
+        return num1 * num2
+
+print(func_executor((sum_numbers, (1, 2)), (multiply_numbers, (2, 4))))
