38d0de74c5dc3eeb18972a08933ae96b357228cf
Emil Kondov <122922427+EmilKondov@users.noreply.github.com>
2024-01-25 16:55:32 +0200

    Create 4.numbers_filter.py

---
+++ Python_Advanced/10.Exercise 6 - Functions Advanced/4.numbers_filter.py
@@ -0,0 +1,17 @@
+def even_odd_filter(**kwargs):
+    for key, value in kwargs.items():
+        if key == "even":
+            list_even = [int(x) for x in value if x % 2 == 0]
+        elif key == "odd":
+            list_odd = [int(x) for x in value if x % 2 != 0]
+
+
+    return 
+
+
+
+
+print(even_odd_filter(
+odd=[1, 2, 3, 4, 10, 5],
+even=[3, 4, 5, 7, 10, 2, 5, 5, 2],
+))
