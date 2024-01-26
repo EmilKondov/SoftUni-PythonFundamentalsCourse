d0c2fd37a31d78b561ca14946a5e5001fc12df7d
Emil Kondov <122922427+EmilKondov@users.noreply.github.com>
2024-01-26 11:36:39 +0200

    Update 6.function_executer.py

--- Python_Advanced/10.Exercise 6 - Functions Advanced/6.function_executer.py
+++ Python_Advanced/10.Exercise 6 - Functions Advanced/6.function_executer.py
@@ -6,14 +6,20 @@
 #     return "\n".join(result)                                                          # за да може съответните аргументи num1, num2 да отидат към съответните параметри
 
 ### Second solution ###
-
+#Това е функцията, която изпълнява тест кода.
+# Чрез цикъл for loop минаваме през tuple от tuples,
+# които представляват KVP и за всяка фунцкия принтраме името
+# и чрез  {func.__name__}, а чрез {func(*args)}
+# извикваме съответната функция и подаваме аргументи като ги разпакетираме чрез *.
+# ако него направим ще изпратим един цял tuple от (num1, num2) в параметъра num1, а за
+# num2 няма да им аргумент и ще ни хвърли error.
 def func_executor(*funcs_data):
     result = []
 
-    return "\n".join([f"{func.__name__} - {func(*args)}" for func, args in funcs_data])
+    return "\n".join([f"{func.__name__} - {func(*args)}" for func, args in funcs_data])  # Идентичен запис, но чрез comprehension
 
 
-
+# Това е зададение тест код.
 def sum_numbers(num1, num2):
     return num1 + num2
 
