02a57b80bb05dde5612e7fcfbead4b4e61b99ed2
Emil Kondov <122922427+EmilKondov@users.noreply.github.com>
2024-01-26 11:27:19 +0200

    Update 6.function_executer.py

--- Python_Advanced/10.Exercise 6 - Functions Advanced/6.function_executer.py
+++ Python_Advanced/10.Exercise 6 - Functions Advanced/6.function_executer.py
@@ -1,11 +1,15 @@
-def func_executor(*action_data):
-    retu
+def func_executor(*funcs_data):
+    result = []
+
+    for func, args in funcs_data:
+        result.append(f"{func.__name__} - {func(*args)}")       # тук разопаковаме args, което е value-to в съотвяетние key-value pair ,
+    return "\n".join(result)                                                          # за да може съответните аргументи num1, num2 да отидат към съответните параметри
 
 
-    def sum_numbers(num1, num2):
-        return num1 + num2
+def sum_numbers(num1, num2):
+    return num1 + num2
 
-    def multiply_numbers(num1, num2):
-        return num1 * num2
+def multiply_numbers(num1, num2):
+    return num1 * num2
 
 print(func_executor((sum_numbers, (1, 2)), (multiply_numbers, (2, 4))))
