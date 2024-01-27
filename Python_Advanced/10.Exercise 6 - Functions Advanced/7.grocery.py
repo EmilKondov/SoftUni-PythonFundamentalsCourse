e7de03320ffe5bea1b4712e097cd0c1b85d4dc35
Emil Kondov <e.kondov96@gmail.com>
2024-01-27 09:12:22 +0200

    Update 7.grocery.py

--- Python_Advanced/10.Exercise 6 - Functions Advanced/7.grocery.py
+++ Python_Advanced/10.Exercise 6 - Functions Advanced/7.grocery.py
@@ -1,12 +1,12 @@
 def grocery_store(**prdocuts):
     sorted_list = sorted(prdocuts.items(), key= lambda x: (-x[1], -len(x[0]), x[0]))
+
     list_of_sorted_products = []
 
-    for product_name, qty in sorted_list:
-        list_of_sorted_products.append(product_name)
-        list_of_sorted_products += str(qty)
+    for product_name, qty in sorted_list:                                       # чрез f стринг добавяме всеки два елемента от sorted_list като единичен стринг и получаваме лист от 3 отделни елемента- стрингове.
+        list_of_sorted_products.append(f"{product_name}: {qty}")
 
-    return  "\n".join([str(x) for x in list_of_sorted_products])
+    return "\n".join(list_of_sorted_products)                                   #принтираме всеки елемент от листа на нов ред
 
 print(grocery_store(
 bread=5,
