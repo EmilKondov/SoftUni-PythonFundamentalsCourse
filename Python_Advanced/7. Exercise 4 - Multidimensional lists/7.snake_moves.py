4ebf5ad0f3743a9ad415be7ec8571f3b1d6d9825
Emil Kondov <122922427+EmilKondov@users.noreply.github.com>
2024-01-29 17:29:32 +0200

    Update 7.snake_moves.py

--- Python_Advanced/7. Exercise 4 - Multidimensional lists/7.snake_moves.py
+++ Python_Advanced/7. Exercise 4 - Multidimensional lists/7.snake_moves.py
@@ -1,21 +1,17 @@
 from collections import deque
 
-rows, cols = [int(x) for x in input().split()]
+rows, columns = [int(x) for x in input().split()]
 
 word = list(input())
 
 word_copy = deque(word)
 
 for row in range(rows):
-    while len(word_copy) < cols:
+    while len(word_copy) < columns:
         word_copy.extend(word)
 
+
     if row % 2 == 0:
-        print(*[word_copy.popleft() for _ in range(cols)], sep="")
+        print(*[word_copy.popleft() for _ in range(columns)], sep="")
     elif row % 2 != 0:
-        print(*[word_copy.popleft() for _ in range(cols)][::-1], sep="")
-
-
-
-# 5 6
-# SoftUni
+        print(*[word_copy.popleft() for _ in range(columns)][::-1], sep="")
