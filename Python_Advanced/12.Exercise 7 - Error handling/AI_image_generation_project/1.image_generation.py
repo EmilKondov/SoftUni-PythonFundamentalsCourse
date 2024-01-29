3d3e7758b442b6825e9351de31d05f2a6d55cd82
Emil Kondov <e.kondov96@gmail.com>
2024-01-30 00:21:00 +0200

    Update 1.image_generation.py

--- Python_Advanced/12.Exercise 7 - Error handling/AI_image_generation_project/1.image_generation.py
+++ Python_Advanced/12.Exercise 7 - Error handling/AI_image_generation_project/1.image_generation.py
@@ -4,25 +4,22 @@ import urllib.request
 import requests
 
 from io import BytesIO
-from PIL import ImageTk
+from PIL import ImageTk, Image
 
 def display_image(image_url: str) -> None:
     with urllib.request.urlopen(image_url) as url:
         image_data = url.read() # тук изтегляме снимката
 
-
     image_stream = BytesIO(image_data)    # взимаме тези данни със самото изображение и създаваме поток от данни (binary symbols) в паметта RAM
-
     image = ImageTk.PhotoImage(Image.open(image_stream))
 
-
-
+    image_label.config(image=image)
+    image_label.image = image # keeps reference to the image
 
 
 
 #В тази функция е връзката ни с API, откъдето получаваме линк към изображението, което AI е генерирал.
 def get_image_url() -> str:
-
     headers = {
         "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMjhiN2UyNzQtNjk1MC00ZTFmLWE1OWMtOGNiNTZhNGFjNDU1IiwidHlwZSI6ImFwaV90b2tlbiJ9.1Q48xkbkr9i6Zkqo8Odhl5E7oyBE7fYOPhas8Y_9pdg"}
 
@@ -39,9 +36,10 @@ def get_image_url() -> str:
     print(result['openai']['items'][0]["image_resource_url"])
 
 
-
-def rened_image():
+def render_image():
+    print("clicked")
     image_url = get_image_url()
+    display_image(image_url)
 
 
 window = tk.Tk()
@@ -51,7 +49,11 @@ window.geometry("500x350") # width x height
 input_field = tk.Entry(window, width=15) # за да го видим трябва да му зададем позиция, което правим на следващия ред
 input_field.place(x=200, y=20)
 
-generate_button = tk.Button(window, text="Create", height=1, command=rened_image)
+
+image_label = tk.Label(window)
+image_label.place(x=125, y=70)
+
+generate_button = tk.Button(window, text="Create", height=1, command=render_image)
 generate_button.place(x= 300, y =17)
 
 window.mainloop()
