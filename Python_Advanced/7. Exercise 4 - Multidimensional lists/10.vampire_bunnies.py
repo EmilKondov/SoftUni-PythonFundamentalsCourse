5c2a2cec0a7ada116e3d9236f5052ca71e42f9a8
Emil Kondov <122922427+EmilKondov@users.noreply.github.com>
2024-01-30 16:24:49 +0200

    Update 10.vampire_bunnies.py

--- Python_Advanced/7. Exercise 4 - Multidimensional lists/10.vampire_bunnies.py
+++ Python_Advanced/7. Exercise 4 - Multidimensional lists/10.vampire_bunnies.py
@@ -3,19 +3,52 @@ def player_position():
         if "P" in matrix[row]:
             return row, matrix[row].index("P")
 
-def check_valid_position():
-    if 0 <= player_movement_row < rows and 0 <= player_movement_column < columns:
-        pass
-    else:
+def check_valid_position(row, col, player=False ):
+    global win
 
+    if 0 <= row < rows and 0 <= col < columns:
+        return True
+    if player:
+        win = True
+
+def bunny_position():
+    bunny_positions_list = []
+
+    for row in range(rows):
+        for col in range(columns):
+            if matrix[row][col] == "B":
+                bunny_positions_list.append([row, col])
+
+    return bunny_positions_list
+
+
+def bunny_move(bunny_position):
+    for row, col in bunny_position:
+        for bunnie_move in directions.values():
+            new_row, new_col = row + bunnie_move[0], col + bunnie_move[1]
+
+            if check_valid_position(new_row, new_col):
+                matrix[new_row][new_col] = "B"
+
+
+def check_player_alive(row, column):
+    if matrix[row][column] == "B":
+        show_results("dead")
+
+def show_results(status="won"):
+    [print(*row, sep="") for row in matrix]
+    print(f"{status}: {player_row} {player_column}")
+
+    raise SystemExit
+
+
+# прочитаме си даните
 rows, columns = [int(x) for x in input().split()]
 matrix = [list(input()) for _ in range(rows)]
+
 commands = input()
+
 win = False
-
-player_row, player_column = player_position()
-matrix[player_row][player_column] = "."
-
 directions = {
     "U": (-1, 0),
     "D": (1, 0),
@@ -23,15 +56,22 @@ directions = {
     "R": (0, 1)
 }
 
+player_row, player_column = player_position()  #запазваме първоначалната позиция на играч
+matrix[player_row][player_column] = "."        #затриваме позицията на играча
+
 for command in commands:
-    if command in directions:
-        player_movement_row, player_movement_column = player_row + directions[command][0], player_column + directions[command][1]
-        if check_valid_position():
+    player_movement_row, player_movement_column = player_row + directions[command][0], player_column + directions[command][1]
+    if check_valid_position(player_movement_row, player_movement_column, True):
+        player_row, player_column = player_movement_row, player_movement_column
+
+    bunny_move(bunny_position())
+
+    if win:
+        show_results()
+
+    check_player_alive(player_row, player_column)
 
 
-
-
-print(matrix)
 
 # 5 6
 # .....P
