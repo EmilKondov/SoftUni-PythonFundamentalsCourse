11264fde26e1cef454fb433c87e6a2637f701a8e
Emil Kondov <e.kondov96@gmail.com>
2024-01-31 23:40:12 +0200

    Update 6..py

--- Python_Advanced/8. Exercise 5 - MDL/Exam_prep_going_through_tasks_again/6..py
+++ Python_Advanced/8. Exercise 5 - MDL/Exam_prep_going_through_tasks_again/6..py
@@ -23,15 +23,15 @@ for row in range(size):
         total_targets += matrix[row].count("x")
 
 count_of_commands = int(input())
-for command in range(count_of_commands):
+for _ in range(count_of_commands):
     command = input().split()
 
     action, direction, *steps = command
 
     if action == "move":
 
-        r = shooter_position[0] + directions[direction][0] * int(*steps)
-        c = shooter_position[1] + directions[direction][1] * int(*steps)
+        r = shooter_position[0] + (directions[direction][0] * int(*steps))
+        c = shooter_position[1] + (directions[direction][1] * int(*steps))
 
 
         if (0 <= r < size and 0 <= c < size) and matrix[r][c] == ".":
@@ -44,16 +44,15 @@ for command in range(count_of_commands):
         shot_col = shooter_position[1] + directions[direction][1]
 
         while 0 <= shot_row < size and 0 <= shot_col < size:
-
             if matrix[shot_row][shot_col] == "x":
-                total_targets -= 1
                 shot_targets += 1
                 shot_targets_coordinates.append([shot_row, shot_col])
+                matrix[shot_row][shot_col] = "."
 
             shot_row += directions[direction][0]
             shot_col += directions[direction][1]
-if shot_targets <= total_targets:
-    print(f"Training not completed! {total_targets} targets left.")
-else:
+if shot_targets < total_targets:
+    print(f"Training not completed! {total_targets - shot_targets} targets left.")
+elif shot_targets == total_targets:
     print(f"Training completed! All {shot_targets} targets hit.")
 print(*shot_targets_coordinates, sep="\n")
