98b3d9b9ba07a46bfa616285912331117456d82c
Emil Kondov <e.kondov96@gmail.com>
2024-02-02 14:39:55 +0200

    Creating folder for current exercise

---
+++ Python_Advanced/14.exercise_8_file_handling/4.directory_traversal.py
@@ -0,0 +1,17 @@
+import os
+
+def save_extensions(dir_name):
+    for filename in os.listdir(dir_name):
+        file = os.path.join(dir_name, filename)
+
+        if os.path.isfile(file):
+            extension = filename.split(".")[-1]
+            extensions[extension] = extensions.get(extension, []) + [filename]
+        elif os.path.isdir(file):
+            save_extensions(file)
+
+
+directory = input()
+extensions = {}
+save_extensions(directory)
+print(extensions)
