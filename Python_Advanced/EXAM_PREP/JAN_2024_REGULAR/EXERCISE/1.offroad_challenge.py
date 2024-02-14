1f75c0a98a2f555c4d0ec7840217f80bd605336c
Emil Kondov <e.kondov96@gmail.com>
2024-02-14 23:45:51 +0200

    Update 1.offroad_challenge.py

--- Python_Advanced/EXAM_PREP/JAN_2024_REGULAR/EXERCISE/1.offroad_challenge.py
+++ Python_Advanced/EXAM_PREP/JAN_2024_REGULAR/EXERCISE/1.offroad_challenge.py
@@ -1,36 +1,42 @@
 from collections import deque
-n = 4
+#defining the number of altitudes
+# N = 4
 
+
+#Read the sequences under the correct type
 fuel_collection = [int(x) for x in input().split()]
 additional_consumption = deque([int(y) for y in input().split()])
 needed_fuel = deque([int(z) for z in input().split()])
 
-altitudes = []
-failed = False
+# altitudes = []
+altitude_count = 0
+# failed = False
 
-for index in range(1, n + 1):
-
+# for index in range(1, N + 1):
+while fuel_collection and additional_consumption and needed_fuel:
     current_fuel = fuel_collection[-1]
     current_consumption = additional_consumption[0]
     current_needed_fuel = needed_fuel[0]
+    difference = current_fuel - current_consumption
 
-    if abs(current_fuel - current_consumption) >= current_needed_fuel:
+    if difference >= current_needed_fuel:
         fuel_collection.pop()
         additional_consumption.popleft()
         needed_fuel.popleft()
-        altitudes.append(f"Altitude {index}")
-        print(f"John has reached: Altitude {index}")
-
-    elif abs(current_fuel - current_consumption) < current_needed_fuel and not altitudes:
-        print("John failed to reach the top.John didn't reach any altitude.")
-        exit()
+        altitude_count += 1
+        # altitudes.append(f"Altitude {index}")
+        print(f"John has reached: Altitude {altitude_count}")
     else:
-        print(f"John did not reach: Altitude {index}")
-        print(f"John failed to reach the top.")
-        failed = True
+        print(f"John did not reach: Altitude {altitude_count + 1}")
+        # failed = True
         break
 
-if failed:
-    print(f"Reached altitudes: {', '.join(altitudes)}")
-else:
-    print(f"John has reached all the altitudes and managed to reach the top!")
+if fuel_collection and altitude_count:
+    print("John failed to reach the top.")
+    print(f"Reached altitudes: {', '.join([f'Altitude {num}' for num in range(1, altitude_count + 1)])}")
+elif fuel_collection and not altitude_count:
+    print("John failed to reach the top.")
+    print("John didn't reach any altitude.")
+
+if not fuel_collection:
+    print("John has reached all the altitudes and managed to reach the top!")
