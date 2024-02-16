e7ed00f526ad4a2689ed525ce6b3fd4ea3b7fad9
Emil Kondov <122922427+EmilKondov@users.noreply.github.com>
2024-02-16 17:42:09 +0200

    Update TEST.py

--- Python_Advanced/TEST.py
+++ Python_Advanced/TEST.py
@@ -6,3 +6,14 @@
 #
 #     return palindrome(word, index -1)
 #
+SIZE = 8
+
+alphabet = ['h', 'g', 'f', 'e', 'd', 'c', 'b', 'a']
+chess_board = ''
+chess_board_matrix = []
+
+for char in alphabet:
+    for num in range(1, SIZE + 1):
+        chess_board += (f"{(char + str(num))} ")
+    chess_board += "\n"
+
