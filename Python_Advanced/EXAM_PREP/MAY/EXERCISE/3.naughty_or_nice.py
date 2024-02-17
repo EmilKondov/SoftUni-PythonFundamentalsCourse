2f4d7ea7b7c80ecf3e3363972e9a29d25542d1a7
Emil Kondov <e.kondov96@gmail.com>
2024-02-17 10:36:37 +0200

    Update 3.naughty_or_nice.py

--- Python_Advanced/EXAM_PREP/MAY/EXERCISE/3.naughty_or_nice.py
+++ Python_Advanced/EXAM_PREP/MAY/EXERCISE/3.naughty_or_nice.py
@@ -1,89 +1,29 @@
-from collections import deque
-
-
-def naughty_or_nice_list(data_list, *args, **kwargs):
-    santa_list  = deque(data_list)
-    commands = args
-    keywords = kwargs
-
+def naughty_or_nice_list(santa_list, *args, **kwargs):
     nice = []
     naughty = []
-    not_found = []
+    result = []
 
-    command_dict = {}
-    for pair in commands:
-        key, value = pair.split("-")
-        command_dict[key] = value
-        
+    def kids_sorting():
+        if len(kids) == 1:
+            nice.append(kids[0][1]) if type_of_kid == "Nice" else naughty.append(kids[0][1])
+            santa_list.remove(kids[0])
 
-    for key in command_dict.keys():
-        if santa_list.count(key) == 1:
-            nice.append(santa_list[key].pop(value))
-        elif str(key) in commands[1]:
-            naughty.append(value)
-        else:
-            not_found.append(value)
 
-       
+    for info in args:
+        number, type_of_kid = info.split("-")
+        kids = [element for element in santa_list if element[0] == int(number)]
+        kids_sorting()
+
+    for name, type_of_kid in kwargs.items():
+        kids = [element for element in santa_list if element[1] == name]
+        kids_sorting()
 
 # OUTPUT
-    result = ''
     if nice:
-        result += f"Nice: {', '.join(nice)}\n"
+        result.append(f"Nice: {', '.join(nice)}")
     if naughty:
-        result += f"Naughty: {','.join(naughty)}\n"
-    if not_found:
-        result += f"Not found: {', '.join(not_found)}\n"
+        result.append(f"Naughty: {', '.join(naughty)}")
+    if santa_list:
+        result.append(f"Not found: {', '.join(k[1] for k in santa_list)}")
 
-    # return result
-
-    
-
-print(naughty_or_nice_list(
-    [
-        (3, "Amy"),
-        (1, "Tom"),
-        (7, "George"),
-        (3, "Katy"),
-    ],
-    "3-Nice",
-    "1-Naughty",
-    Amy="Nice",
-    Katy="Naughty",
-))
-
-
-# print(naughty_or_nice_list(
-#  [
-#  (7, "Peter"),
-#  (1, "Lilly"),
-#  (2, "Peter"),
-#  (12, "Peter"),
-#  (3, "Simon"),
-#  ],
-#  "3-Nice",
-#  "5-Naughty",
-#  "2-Nice",
-#  "1-Nice",
-#  ))
-#
-#
-# print(naughty_or_nice_list(
-#  [
-#  (6, "John"),
-#  (4, "Karen"),
-#  (2, "Tim"),
-#  (1, "Merry"),
-#  (6, "Frank"),
-#  ],
-#  "6-Nice",
-#  "5-Naughty",
-#  "4-Nice",
-#  "3-Naughty",
-#  "2-Nice",
-#  "1-Naughty",
-#  Frank="Nice",
-#  Merry="Nice",
-#  John="Naughty",
-# ))
-#
+    return '\n'.join(result)
