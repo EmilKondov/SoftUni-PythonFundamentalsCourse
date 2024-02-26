197e317c79325b1434b4bdb613932b1c3cbaefe5
Emil Kondov <e.kondov96@gmail.com>
2024-02-26 22:11:38 +0200

    Project_task_05
    To be completed

---
+++ python_oop/class_objects_exercise/05_Task/project/section.py
@@ -0,0 +1,19 @@
+from typing import List
+from project.task import Task
+
+class Section:
+    def __init__(self, name: int, ):
+        self.name = name
+
+    def add_task(self, new_task: Task):
+        
+
+    def complete_task(self, task_name: str):
+        pass
+
+    def clean_section(self):
+        pass
+
+    def view_section(self):
+        pass
+
