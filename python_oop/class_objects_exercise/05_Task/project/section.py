0a1546aaba2292dde9d76a69dd36049585bc455d
Emil Kondov <122922427+EmilKondov@users.noreply.github.com>
2024-02-27 17:16:44 +0200

    Update section.py

--- python_oop/class_objects_exercise/05_Task/project/section.py
+++ python_oop/class_objects_exercise/05_Task/project/section.py
@@ -13,14 +13,29 @@ class Section:
         return f"Task {new_task.details()} is added to the section"
 
     def complete_task(self, task_name: str):
-        if task_name not in self.tasks:
+        try:
+            task = next(filter(lambda t: t.name == task_name, self.tasks))
+        except StopIteration:
             return f"Could not find task with the name {task_name}"
 
-        Task.
+        task.completed = True
+        return f"Completed task {task_name}"
+
 
     def clean_section(self):
-        pass
+        count_of_tasks = 0
+
+        for task in self.tasks:
+            if task.completed:
+                count_of_tasks += 1
+                self.tasks.remove(task)
+
+        return f"Cleared {count_of_tasks} tasks."
+
 
     def view_section(self):
-        pass
+        tasks_details = "\n".join(t.details() for t in self.tasks)
+        return f"Section {self.name}:\n" \
+               f"{tasks_details}"
+
 
