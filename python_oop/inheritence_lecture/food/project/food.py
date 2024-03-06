6556f9c7719e70e89901d4bbec3abfe944aa64fa
Emil Kondov <e.kondov96@gmail.com>
2024-03-06 17:53:12 +0200

    Project1

--- python_oop/inheritence_lecture/food/project/food.py
+++ python_oop/inheritence_lecture/food/project/food.py
@@ -1,8 +1,3 @@
 class Food:
     def __init__(self, expiration_date: str):
         self.expiration_date = expiration_date
-
-class Fruit(Food):
-    def __init__(self, name, expiration_date):
-        super().__init__(expiration_date)
-        self.name = name
--- python_oop/inheritence_lecture/food/project/fruit.py
+++ python_oop/inheritence_lecture/food/project/fruit.py
@@ -1,4 +1,7 @@
-class Fruit:
+from project.food import Food
+
+
+class Fruit(Food):
     def __init__(self, name: str, expiration_date: str):
         self.name = name
-        self.expiration_date = expiration_date
+        super().__init__(expiration_date)
