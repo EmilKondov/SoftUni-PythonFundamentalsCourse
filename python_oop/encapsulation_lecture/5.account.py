43f83a3a85dd4cb1b69528536cf54cd12690d4dd
Emil Kondov <e.kondov96@gmail.com>
2024-03-12 00:00:48 +0200

    Zoo

--- python_oop/encapsulation_lecture/5.account.py
+++ python_oop/encapsulation_lecture/5.account.py
@@ -6,7 +6,7 @@ class Account:
 
     def get_id(self, pin: int):
         if pin == self.__pin:
-            return  self.__id
+            return self.__id
         return "Wrong pin"
 
     def change_pin(self, old_pin: int, new_pin: int):
