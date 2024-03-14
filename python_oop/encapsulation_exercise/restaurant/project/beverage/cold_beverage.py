e18b0b1677fc580b4bac5f2cdb53dcd9b9577b35
Emil Kondov <e.kondov96@gmail.com>
2024-03-14 18:53:11 +0200

    Project 4  - restaurant

---
+++ python_oop/encapsulation_exercise/restaurant/project/beverage/cold_beverage.py
@@ -0,0 +1,12 @@
+from project.beverage.beverage import Beverage
+
+
+class ColdBeverage(Beverage):
+    pass
+
+
+"""
+Тук казваме, съществува  такъв клас (ColdBeverage), който наследява клас (Beverage). 
+Тъй като нямаме логика тук и нищо друго не се извършва пишем pass и го оставяме така.
+Същото се отнася и за HotBeverage.
+"""
