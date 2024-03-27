8280adcbc4311f0dc9c68a1ede7fa0d009ebd415
Emil Kondov <122922427+EmilKondov@users.noreply.github.com>
2024-03-27 16:38:29 +0200

    .

--- python_oop/solid_lecture/01_SRP/books.py
+++ python_oop/solid_lecture/01_SRP/books.py
@@ -1,9 +1,17 @@
 class Book:
-    def __init__(self, title, author, location):
+    def __init__(self, title, author):
         self.title = title
         self.author = author
-        self.location = location
         self.page = 0
 
     def turn_page(self, page):
         self.page = page
+
+class Library:
+    def __init__(self, books):
+        self.books = books
+
+
+    def find_book(self, title):
+        
+
