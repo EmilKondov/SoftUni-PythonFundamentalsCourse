d932c56aec02a0e208b07f392bb7aa481ba36d2d
Emil Kondov <e.kondov96@gmail.com>
2024-03-27 20:33:10 +0200

    Update
    Not completed

---
+++ python_oop/Exam_preparation/april_2021_session_1_lecture/project/hardware/power_hardware.py
@@ -0,0 +1,8 @@
+from math import floor
+
+from project.hardware.hardware import Hardware
+
+
+class PowerHardware(Hardware):
+    def __init__(self, name: str, hardware_type: str, capacity: int, memory: int):
+        super().__init__(name, "Power", int(capacity * 0.25), int(memory * 0.75))
