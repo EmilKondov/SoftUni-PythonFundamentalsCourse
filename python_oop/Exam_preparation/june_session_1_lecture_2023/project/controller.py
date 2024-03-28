01fe3eab57f72bc8102dd7e1c7ed87a7b1e25bcb
Emil Kondov <e.kondov96@gmail.com>
2024-03-28 21:45:43 +0200

    Exam preparation June 2023
    To complete the last class

--- python_oop/Exam_preparation/june_session_1_lecture_2023/project/controller.py
+++ python_oop/Exam_preparation/june_session_1_lecture_2023/project/controller.py
@@ -1,12 +1,14 @@
-from typing import List
+from typing import List, Tuple
 
 from project.player import Player
 from project.supply.supply import Supply
 
 
 class Controller:
-    def __init__(self, players: list, supplies: list):
+    def __init__(self):
         self.players: List[Player] = []
         self.supplies: List[Supply] = []
 
-    def add_player(self, player1: Player, player2: Player, … playerN: Player)
+    def add_player(self, *players: Tuple[Player]) -> str :
+        players_added = []
+        
