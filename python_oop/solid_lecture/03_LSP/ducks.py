3f87a435fec31639ccbb790a6d2ee472d0314b41
Emil Kondov <122922427+EmilKondov@users.noreply.github.com>
2024-04-20 00:20:20 +0300

    Update ducks.py

---
+++ python_oop/solid_lecture/03_LSP/ducks.py
@@ -0,0 +1,63 @@
+from abc import abstractmethod, ABC
+
+
+class Duck(ABC):
+    @staticmethod
+    @abstractmethod
+    def quack():
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def walk():
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def fly():
+        pass
+
+
+class RubberDuck:
+    @staticmethod
+    def quack():
+        return "Squeek"
+
+
+class RobotDuck(Duck):
+    HEIGHT = 50
+
+    def __init__(self):
+        self.height = 0
+
+    @staticmethod
+    def quack():
+        return 'Robotic quacking'
+
+    @staticmethod
+    def walk():
+        return 'Robotic walking'
+
+    def fly(self):
+        """can only fly to specific height but
+        when it reaches it starts landing automatically"""
+        if self.height == self.HEIGHT:
+            self.land()
+        else:
+            self.height += 1
+
+    def land(self):
+        self.height = 0
+
+
+
+
+rb = RubberDuck
+print(rb.quack())
+
+duck = Duck
+print(duck.quack())
+print(duck.fly())
+
+robot_duck = RobotDuck
+print(robot_duck.fly(50))
