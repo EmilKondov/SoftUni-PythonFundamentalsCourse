5efccc0f98d8b6fabfb56d2d3ac561cf97dd9b9d
Emil Kondov <122922427+EmilKondov@users.noreply.github.com>
2024-04-21 15:16:41 +0300

    .

--- python_oop/iterators_generators_lecture/main.py
+++ python_oop/iterators_generators_lecture/main.py
@@ -1,10 +1,28 @@
-my_list = [4, 7, 0, 3]
-# get an iterator using iter()
-my_iter = iter(my_list)
-print(next(my_iter)) # 4
-print(next(my_iter)) # 7
-print(my_iter.__next__()) # 0
-print(my_iter.__next__()) # 3
-try:
-    next(my_iter)
-ex
+# my_list = [4, 7, 0, 3]
+# # get an iterator using iter()
+# my_iter = iter(my_list)
+# print(next(my_iter)) # 4
+# print(next(my_iter)) # 7
+# print(my_iter.__next__()) # 0
+# print(my_iter.__next__()) # 3
+# try:
+#     next(my_iter)
+# except StopIteration:
+#     print("No more items")
+#
+# iterable = input()
+# iter_obj = iter(iterable)
+# while True:
+#     try:
+#         element = next(iter_obj) # get the next item
+#         print(element)
+# # do something with element
+#     except StopIteration:
+# # if StopIteration is raised, break from loop
+#         break
+#
+
+
+for i in range(10):
+    print(i)
+
