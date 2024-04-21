425dd3cfd296b82eceee247b8b51305be1baf2ed
Emil Kondov <122922427+EmilKondov@users.noreply.github.com>
2024-04-21 12:24:29 +0300

    Iterators and generators

---
+++ python_oop/iterators_generators_lecture/main.py
@@ -0,0 +1,10 @@
+my_list = [4, 7, 0, 3]
+# get an iterator using iter()
+my_iter = iter(my_list)
+print(next(my_iter)) # 4
+print(next(my_iter)) # 7
+print(my_iter.__next__()) # 0
+print(my_iter.__next__()) # 3
+try:
+    next(my_iter)
+ex
